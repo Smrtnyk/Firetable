f78a2bfeb0e54e882abf280a93dabd7aaedc6330
Milan <milankeser@outlook.de>
2021-05-02 20:37:47 +0200

    fix(PageHome): get rid of useless code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useStore } from "src/store";
 import { computed, defineComponent } from "vue";
 import { tryCatchLoadingWrapper } from "src/helpers/ui-helpers";
 import { logoutUser } from "src/services/firebase/auth";
@@ -10,18 +9,6 @@ export default defineComponent({
     name: "App",
     setup() {
         const q = useQuasar();
-        const store = useStore();
-        const user = computed(() => store.state.auth.user);
-        const isReady = computed<boolean | null>(
-            () => store.state.auth.isReady
-        );
-        const isAuthenticated = computed<boolean | null>(
-            () => store.state.auth.isAuthenticated
-        );
-
-        function onLogoutUser() {
-            void tryCatchLoadingWrapper(() => logoutUser());
-        }
 
         q.iconMapFn = (iconName: string): { icon: string } | void => {
             const icon = myIcons[iconName];
@@ -32,19 +19,6 @@ export default defineComponent({
 
         q.dark.set(getDarkMode());
 
-        return () => {
-            if ((user.value && isReady.value) || !isAuthenticated.value) {
-                return <router-view />;
-            }
-
-            if (isReady.value && !user.value) {
-                return (
-                    <div>
-                        <h1>Something bad happened</h1>
-                        <q-btn onClick={onLogoutUser}> logout </q-btn>
-                    </div>
-                );
-            }
-        };
+        return () => <router-view />;
     },
 });
