3555269c7fe67504781242de1f64b3fb82ddbb14
Milan <milankeser@outlook.de>
2021-06-16 19:47:39 +0200

    chore: remove unnecessary animations props in quasar.conf.js

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -104,7 +104,7 @@ module.exports = configure(function (ctx) {
         },
 
         // animations: 'all', // --- includes all animations
-        animations: ["fadeIn", "fadeOut"],
+        animations: [],
 
         ssr: { pwa: false },
 
