c86d8b9ff374a84d00bd19863975b7136e11c84c
Milan <milankeser@outlook.de>
2021-10-20 22:02:04 +0200

    chore: improve vue shim

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,6 +1,7 @@
 // Mocks all files ending in `.vue` showing them as plain Vue instances
 declare module "*.vue" {
-    import { ComponentOptions } from "vue";
-    const component: ComponentOptions;
+    import type { DefineComponent } from "vue";
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>;
     export default component;
 }
