6545d95cc633beaed2b70602bac6fb51dfe6b0b3
Milan <milankeser@outlook.de>
2021-10-26 21:54:44 +0200

    chore(ImageTools): simplify implementation

--- frontend/src/helpers/ImageTools.ts
+++ frontend/src/helpers/ImageTools.ts
@@ -1,38 +1,3 @@
-const hasBlobConstructor =
-    typeof Blob !== "undefined" &&
-    (function () {
-        try {
-            return Boolean(new Blob());
-        } catch (e) {
-            return false;
-        }
-    })();
-
-const hasArrayBufferViewSupport =
-    hasBlobConstructor &&
-    typeof Uint8Array !== "undefined" &&
-    (function () {
-        try {
-            return new Blob([new Uint8Array(100)]).size === 100;
-        } catch (e) {
-            return false;
-        }
-    })();
-
-const hasToBlobSupport =
-    typeof HTMLCanvasElement !== "undefined"
-        ? HTMLCanvasElement.prototype.toBlob
-        : false;
-
-const hasBlobSupport =
-    hasToBlobSupport ||
-    (typeof Uint8Array !== "undefined" &&
-        typeof ArrayBuffer !== "undefined" &&
-        typeof atob !== "undefined");
-
-const hasReaderSupport =
-    typeof FileReader !== "undefined" || typeof URL !== "undefined";
-
 export class ImageTools {
     static resize(
         file: File,
@@ -73,7 +38,7 @@ export class ImageTools {
         }
 
         // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
-        if (!ImageTools.isSupported() || !file.type.match(/image.*/)) {
+        if (!file.type.match(/image.*/)) {
             callback(file, false);
         }
 
@@ -116,70 +81,25 @@ export class ImageTools {
             ctx.imageSmoothingQuality = "high";
             ctx.drawImage(image, 0, 0, width, height);
 
-            if (hasToBlobSupport) {
-                canvas.toBlob((blob) => {
-                    if (!blob) return;
-                    callback(blob, true);
-                }, file.type);
-            } else {
-                const blob = ImageTools._toBlob(canvas, file.type);
+            canvas.toBlob((blob) => {
+                if (!blob) return;
                 callback(blob, true);
-            }
+            }, file.type);
         };
 
-        ImageTools._loadImage(image, file);
+        loadImage(image, file);
     }
+}
 
-    static _toBlob(canvas: HTMLCanvasElement, type: string) {
-        const dataURI = canvas.toDataURL(type);
-        const dataURIParts = dataURI.split(",");
-        let byteString;
-        if (dataURIParts[0].indexOf("base64") >= 0) {
-            // Convert base64 to raw binary data held in a string:
-            byteString = atob(dataURIParts[1]);
-        } else {
-            // Convert base64/URLEncoded data component to raw binary data:
-            byteString = decodeURIComponent(dataURIParts[1]);
-        }
-        const arrayBuffer = new ArrayBuffer(byteString.length);
-        const intArray = new Uint8Array(arrayBuffer);
-
-        for (let i = 0; i < byteString.length; i += 1) {
-            intArray[i] = byteString.charCodeAt(i);
-        }
-
-        const mimeString = dataURIParts[0].split(":")[1].split(";")[0];
-        let blob;
-
-        if (hasBlobConstructor) {
-            blob = new Blob(
-                [hasArrayBufferViewSupport ? intArray : arrayBuffer],
-                { type: mimeString }
-            );
-        } else {
-            blob = new Blob([arrayBuffer]);
-        }
-        return blob;
-    }
-
-    static _loadImage(image: HTMLImageElement, file: Blob) {
-        if (typeof URL === "undefined") {
-            const reader = new FileReader();
-            reader.onload = function (evt) {
-                if (!evt.target?.result) return;
-                image.src = evt.target.result as string;
-            };
-            reader.readAsDataURL(file);
-        } else {
-            image.src = URL.createObjectURL(file);
-        }
-    }
-
-    static isSupported() {
-        return (
-            typeof HTMLCanvasElement !== "undefined" &&
-            hasBlobSupport &&
-            hasReaderSupport
-        );
+function loadImage(image: HTMLImageElement, file: Blob) {
+    if (typeof URL === "undefined") {
+        const reader = new FileReader();
+        reader.onload = function (evt) {
+            if (!evt.target?.result) return;
+            image.src = evt.target.result as string;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        image.src = URL.createObjectURL(file);
     }
 }
