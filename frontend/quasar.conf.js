d4231156b600d9548bfc56476c4574fa2cde4ad0
Smrtnyk <milankeser@outlook.de>
2021-11-05 17:37:17 +0100

    quasar.conf.js: don't use http2 and https for now

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -72,13 +72,12 @@ module.exports = configure(function (ctx) {
         },
 
         devServer: {
-            http2: true,
-            https: ctx.dev
-                ? {
-                      key: fs.readFileSync("./key.pem"),
-                      cert: fs.readFileSync("./cert.pem"),
-                  }
-                : true,
+            // https: ctx.dev
+            //     ? {
+            //           key: fs.readFileSync("./key.pem"),
+            //           cert: fs.readFileSync("./cert.pem"),
+            //       }
+            //     : true,
             port: 8080,
             open: true,
         },
