dd5c3331f25b4040b892c4846ff4e7e533cc8719
Smrtnyk <milankeser@outlook.de>
2021-11-12 17:40:54 +0100

    events-store: remove unused flag

--- frontend/src/stores/events-store.ts
+++ frontend/src/stores/events-store.ts
@@ -2,16 +2,12 @@ import { defineStore } from "pinia";
 
 export const useEventsStore = defineStore("events", {
     state: () => ({
-        showCreateEventModal: false,
         showEventGuestListDrawer: false,
         showAddNewGuestForm: false,
     }),
     actions: {
         toggleEventGuestListDrawerVisibility() {
             this.showEventGuestListDrawer = !this.showEventGuestListDrawer;
-        },
-        toggleEventCreateModalVisibility() {
-            this.showCreateEventModal = !this.showCreateEventModal;
         },
         toggleShowAddNewGuestFormVisibility() {
             this.showAddNewGuestForm = !this.showAddNewGuestForm;
