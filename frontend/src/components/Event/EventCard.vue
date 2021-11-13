87dfe7c0b007356ff31b190656121596545c536d
Smrtnyk <milankeser@outlook.de>
2021-11-13 12:25:16 +0100

    EventCard: redesign

--- frontend/src/components/Event/EventCard.vue
+++ frontend/src/components/Event/EventCard.vue
@@ -11,7 +11,7 @@ const props = defineProps<Props>();
 
 <template>
     <router-link :to="{ name: 'event', params: { id: props.event.id } }">
-        <div class="news-card">
+        <div class="card">
             <div class="event-success-indicator" v-if="props.event.reservedPercentage >= 75">
                 <q-icon name="fire" color="warning" class="bg-warning-shadow rounded" size="md" />
                 <q-tooltip>
@@ -19,100 +19,156 @@ const props = defineProps<Props>();
                     performing well!
                 </q-tooltip>
             </div>
-            <q-img
-                basic
-                :src="props.event.img || 'images/default-event-img.jpg'"
-                alt=""
-                class="news-card__image"
-                :ratio="1"
-            />
-            <div class="news-card__text-wrapper">
-                <h3 class="news-card__title">{{ props.event.name }}</h3>
-                <div class="news-card__post-date">
-                    <q-icon
-                        name="calendar"
-                        color="white"
-                        class="gradient-warning q-pa-xs rounded"
-                        size="xs"
-                    />
-                    {{ dateFromTimestamp(props.event.date) }}
-                    <q-icon
-                        name="clock"
-                        color="white"
-                        class="q-ml-sm gradient-positive q-pa-xs rounded"
-                        size="xs"
-                    />
-                    {{ hourFromTimestamp(props.event.date) }}
+            <div class="card__image-container">
+                <img
+                    class="card__image"
+                    :src="props.event.img || 'images/default-event-img.jpg'"
+                    alt=""
+                />
+            </div>
 
-                    <q-icon
-                        name="euro"
-                        color="white"
-                        class="q-ml-sm gradient-pink q-pa-xs rounded"
-                        size="xs"
-                    />
-                    {{ props.event.entryPrice || "Free" }}
-                </div>
+            <svg class="card__svg" viewBox="0 0 800 500">
+                <path
+                    d="M 0 160 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 400 800 300 L 800 500 L 0 500"
+                    stroke="transparent"
+                    fill="#333"
+                />
+                <path
+                    class="card__line"
+                    d="M 0 160 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 400 800 300"
+                    stroke="pink"
+                    stroke-width="3"
+                    fill="transparent"
+                />
+            </svg>
+
+            <div class="card__content">
+                <h2 class="text-h3 q-ma-none">{{ props.event.name }}</h2>
+                <q-icon
+                    name="calendar"
+                    color="white"
+                    class="gradient-warning q-pa-xs rounded"
+                    size="xs"
+                />
+                {{ dateFromTimestamp(props.event.date) }}
+
+                <q-icon
+                    name="clock"
+                    color="white"
+                    class="q-ml-sm gradient-positive q-pa-xs rounded"
+                    size="xs"
+                />
+                {{ hourFromTimestamp(props.event.date) }}
+
+                <q-icon
+                    name="euro"
+                    color="white"
+                    class="q-ml-sm gradient-pink q-pa-xs rounded"
+                    size="xs"
+                />
+                {{ props.event.entryPrice || "Free" }}
             </div>
         </div>
     </router-link>
 </template>
 
 <style lang="scss">
-.news-card {
-    border: 0px solid aqua;
-    position: relative;
-    border-radius: 0.5rem;
-    flex: 1;
+img {
+    max-width: 100%;
     width: 100%;
-    overflow: hidden;
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-    transform: translate3d(0, 0, 0);
 }
 
-.news-card::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0)
-        linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 80%);
-    z-index: 0;
-}
-
-.news-card__image {
-    transition: transform 3s ease;
+.card {
     position: relative;
-    z-index: -1;
-}
-
-.news-card__text-wrapper {
-    right: 0;
-    left: 0;
-    position: absolute;
-    bottom: 0;
+    background: #333;
+    width: 100%;
+    border-radius: 6px;
     padding: 1rem;
-    color: white;
-    /*     background-color: rgba(0, 0, 0, 0.4); */
-    transition: background-color 1.5s ease;
+    color: #aaa;
+    box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 0.2), 0 0 1rem rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+
+    .event-success-indicator {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        z-index: 2;
+    }
+
+    &__image-container {
+        margin: -2rem -2rem 1rem -2rem;
+    }
+
+    &__line {
+        opacity: 0;
+        animation: LineFadeIn 0.8s 0.8s forwards ease-in;
+    }
+
+    &__image {
+        opacity: 0;
+        animation: ImageFadeIn 0.8s 1.4s forwards;
+    }
+
+    &__content {
+        margin-top: -1rem;
+        opacity: 0;
+        animation: ContentFadeIn 0.8s 1.6s forwards;
+    }
+
+    &__svg {
+        position: absolute;
+        left: 0;
+        top: 125px;
+    }
 }
 
-.news-card__title {
-    transition: color 1s ease;
-    margin-bottom: 0.5rem;
+@keyframes LineFadeIn {
+    0% {
+        opacity: 0;
+        d: path("M 0 300 Q 0 300 0 300 Q 0 300 0 300 C 0 300 0 300 0 300 Q 0 300 0 300 ");
+        stroke: #fff;
+    }
+    50% {
+        opacity: 1;
+        d: path(
+            "M 0 300 Q 50 300 100 300 Q 250 300 350 300 C 350 300 500 300 650 300 Q 750 300 800 300"
+        );
+        stroke: #888bff;
+    }
+    100% {
+        opacity: 1;
+        d: path(
+            "M -2 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 802 400"
+        );
+        stroke: #545581;
+    }
 }
 
-.news-card__post-date {
-    font-size: 1rem;
-    margin-bottom: 0.5rem;
-    color: #ccc;
+@keyframes ContentFadeIn {
+    0% {
+        transform: translateY(-1rem);
+        opacity: 0;
+    }
+    100% {
+        transform: translateY(0);
+        opacity: 1;
+    }
 }
 
-.event-success-indicator {
-    position: absolute;
-    top: 5px;
-    right: 5px;
+@keyframes ImageFadeIn {
+    0% {
+        transform: translate(-0.5rem, -0.5rem) scale(1.05);
+        opacity: 0;
+        filter: blur(2px);
+    }
+    50% {
+        opacity: 1;
+        filter: blur(2px);
+    }
+    100% {
+        transform: translateY(0) scale(1);
+        opacity: 1;
+        filter: blur(0);
+    }
 }
 </style>
