4ea0635359fb33f21a082ba908d090228f967052
Milan Keser <31451544+Smrtnyk@users.noreply.github.com>
2022-08-20 09:33:58 +0200

    chore(frontend): clean quasar.config.js

--- packages/frontend/quasar.config.js
+++ packages/frontend/quasar.config.js
@@ -1,10 +1,5 @@
 /* eslint-env node */
 
-/*
- * This file runs in a Node context (it's NOT transpiled by Babel), so use only
- * the ES6 features that are supported by your Node version. https://node.green/
- */
-
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -14,7 +9,7 @@ module.exports = configure(function (/* ctx */) {
     return {
         eslint: {
             // fix: true,
-            // include = [],
+            // include: [],
             // exclude: [],
             rawOptions: {
                 cache: false,
@@ -66,7 +61,6 @@ module.exports = configure(function (/* ctx */) {
 
             // publicPath: '/',
             // analyze: true,
-            // env: {},
             // rawDefine: {}
             // ignorePublicFolder: true,
             // minify: false,
@@ -131,27 +125,6 @@ module.exports = configure(function (/* ctx */) {
         //   electronPreload: 'src-electron/electron-preload'
         // },
 
-        // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
-        ssr: {
-            // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-            // will mess up SSR
-
-            // extendSSRWebserverConf (esbuildConf) {},
-            // extendPackageJson (json) {},
-
-            pwa: false,
-
-            // manualStoreHydration: true,
-            // manualPostHydrationTrigger: true,
-
-            prodPort: 3000, // The default port that the production server should use
-            // (gets superseded if process.env.PORT is specified at runtime)
-
-            middlewares: [
-                "render", // keep this as last one
-            ],
-        },
-
         // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
         pwa: {
             workboxPluginMode: "InjectManifest",
@@ -192,51 +165,6 @@ module.exports = configure(function (/* ctx */) {
                     },
                 ],
             },
-        },
-
-        // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
-        cordova: {
-            // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-        },
-
-        // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
-        capacitor: {
-            hideSplashscreen: true,
-        },
-
-        // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
-        electron: {
-            // extendElectronMainConf (esbuildConf)
-            // extendElectronPreloadConf (esbuildConf)
-
-            inspectPort: 5858,
-
-            bundler: "packager", // 'packager' or 'builder'
-
-            packager: {
-                // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-                // OS X / Mac App Store
-                // appBundleId: '',
-                // appCategoryType: '',
-                // osxSign: '',
-                // protocol: 'myapp://path',
-                // Windows only
-                // win32metadata: { ... }
-            },
-
-            builder: {
-                // https://www.electron.build/configuration/configuration
-
-                appId: "vite-example",
-            },
-        },
-
-        // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
-        bex: {
-            contentScripts: ["my-content-script"],
-
-            // extendBexScriptsConf (esbuildConf) {}
-            // extendBexManifestJson (json) {}
         },
     };
 });
