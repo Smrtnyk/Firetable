f01c601a81a2d981965090c9fc6af7f1d53664b1
Smrtnyk <milankeser@outlook.de>
2023-02-26 14:23:51 +0100

    chore: use memoize for firebase init

--- packages/backend/src/firebase/base.ts
+++ packages/backend/src/firebase/base.ts
@@ -3,21 +3,19 @@ import { getAuth, connectAuthEmulator, Auth } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator, Firestore } from "firebase/firestore";
 import { getFunctions, connectFunctionsEmulator, Functions } from "firebase/functions";
 import fbConfig from "./fb-config.json";
+import { memoize } from "@firetable/utils";
 
-let initialized = false;
-
-export function initializeFirebase() {
+export const initializeFirebase = memoize(() => {
     const firebaseApp = initializeApp(fbConfig);
     const firestore = getFirestore(firebaseApp);
     const functions = getFunctions(firebaseApp, "europe-west3");
     const auth = getAuth(firebaseApp);
     initEmulators(firestore, auth, functions);
-    initialized = true;
     return { firestore, auth, functions, firebaseApp };
-}
+});
 
 function initEmulators(firestore: Firestore, auth: Auth, functions: Functions): void {
-    if (location.hostname !== "localhost" || initialized) {
+    if (location.hostname !== "localhost") {
         return;
     }
     connectAuthEmulator(auth, "http://localhost:9099/", { disableWarnings: true });
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,11 +1,12 @@
 import { matchesValue } from "./matches-value";
+import { memoize } from "./memoize";
 import { not } from "./not";
 import { propIsTruthy } from "./prop-is-truthy";
 import { takeLast } from "./take-last";
 import { takeProp } from "./take-prop";
 
 export * from "./type-guards";
-export { takeLast, not, propIsTruthy, takeProp, matchesValue };
+export { takeLast, not, propIsTruthy, takeProp, matchesValue, memoize };
 
 export function NOOP() {
     /* EMPTY */
