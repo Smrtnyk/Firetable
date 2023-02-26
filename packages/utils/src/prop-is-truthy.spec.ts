74d728281e2f1717072fad0ace3871f5d109085f
Smrtnyk <milankeser@outlook.de>
2023-02-26 19:22:50 +0100

    chore: add tests for propIsTruthy helper

---
+++ packages/utils/src/prop-is-truthy.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "@jest/globals";
+import { propIsTruthy } from "./prop-is-truthy";
+
+describe("propIsTruthy", () => {
+    const obj = {
+        a: true,
+        b: false,
+        c: 0,
+        d: "",
+        e: null,
+        f: undefined,
+    };
+
+    it("returns true when the property value is truthy", () => {
+        expect(propIsTruthy("a")(obj)).toBe(true);
+    });
+
+    it("returns false when the property value is falsy", () => {
+        expect(propIsTruthy("b")(obj)).toBe(false);
+        expect(propIsTruthy("c")(obj)).toBe(false);
+        expect(propIsTruthy("d")(obj)).toBe(false);
+        expect(propIsTruthy("e")(obj)).toBe(false);
+        expect(propIsTruthy("f")(obj)).toBe(false);
+    });
+
+    it("returns false when the property is undefined", () => {
+        expect(propIsTruthy("g")(obj)).toBe(false);
+    });
+});
