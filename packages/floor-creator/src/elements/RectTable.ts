5c0da5ba7424eb1971cf087679d8bf251c15500e
Smrtnyk <milankeser@outlook.de>
2023-09-30 21:40:09 +0200

    chore(floor-editor): add limitations to RectTable

--- packages/floor-creator/src/elements/RectTable.ts
+++ packages/floor-creator/src/elements/RectTable.ts
@@ -21,6 +21,10 @@ export class RectTable extends fabric.Group {
     reservation: Reservation | null = null;
     label: string;
     private isAnimating: boolean = false;
+    private readonly initialStrokeWidth: number;
+    private readonly initialFontSize: number;
+    private readonly initialWidth: number;
+    private readonly initialHeight: number;
 
     constructor(options: RectTableElementOptions) {
         const fill = determineTableColor(options.groupOptions.reservation);
@@ -30,19 +34,63 @@ export class RectTable extends fabric.Group {
             stroke: "black",
             strokeWidth: 2,
         });
+
         const textLabel = new fabric.Text(options.groupOptions.label, {
             fontSize: FONT_SIZE,
             fill: TABLE_TEXT_FILL_COLOR,
             textAlign: "center",
             originX: "center",
             originY: "center",
-            left: tableRect.left! + tableRect.width! / 2, // Adjust as necessary
-            top: tableRect.top! + tableRect.height! / 2, // Adjust as necessary
+            left: tableRect.left! + tableRect.width! / 2,
+            top: tableRect.top! + tableRect.height! / 2,
         });
+
         super([tableRect, textLabel], options.groupOptions);
 
+        this.initialWidth = tableRect.width!;
+        this.initialHeight = tableRect.height!;
         this.label = options.groupOptions.label;
         if (options.groupOptions.reservation) this.reservation = options.groupOptions.reservation;
+
+        this.initialStrokeWidth = tableRect.strokeWidth || 2;
+        this.initialFontSize = textLabel.fontSize || FONT_SIZE;
+
+        this.on("scaling", this.handleScaling.bind(this));
+    }
+
+    private handleScaling(): void {
+        this.enforceStrokeWidth();
+        this.resetFontSize();
+        this.enforceMinimumDimensions();
+    }
+
+    private enforceMinimumDimensions(): void {
+        if (!this.scaleX || !this.scaleY) return;
+
+        if (this.scaleX * this.initialWidth < 50) {
+            this.scaleX = 50 / this.initialWidth;
+        }
+
+        if (this.scaleY * this.initialHeight < 50) {
+            this.scaleY = 50 / this.initialHeight;
+        }
+    }
+
+    private enforceStrokeWidth(): void {
+        const tableRect = this.item(0) as fabric.Rect;
+        if (!this.scaleX || !this.scaleY) return;
+        tableRect.set({
+            strokeWidth: this.initialStrokeWidth / Math.max(this.scaleX, this.scaleY),
+        });
+    }
+
+    private resetFontSize(): void {
+        const textLabel = this.item(1) as unknown as fabric.Text;
+        textLabel.set({
+            fontSize: this.initialFontSize,
+            left: (this.item(0) as fabric.Rect).left! + (this.item(0) as fabric.Rect).width! / 2, // Adjust position if necessary
+            top: (this.item(0) as fabric.Rect).top! + (this.item(0) as fabric.Rect).height! / 2, // Adjust position if necessary
+        });
     }
 
     startSmoothBlinking() {
