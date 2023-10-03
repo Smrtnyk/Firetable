3af5485200b81c04192f6994aa37a9657a3dd585
Smrtnyk <milankeser@outlook.de>
2023-10-03 19:18:55 +0200

    chore(Floor): refactor drawGrid

--- packages/floor-creator/src/Floor.ts
+++ packages/floor-creator/src/Floor.ts
@@ -263,14 +263,32 @@ export class Floor {
         const height = this.floorDoc.height;
         const left = (width % gridSize) / 2;
         const top = (height % gridSize) / 2;
-        const lines = [];
+
+        const lines = this.createGridLines(width, height, gridSize, left, top);
+        this.addGridToCanvas(lines);
+    }
+
+    private createGridLines(
+        width: number,
+        height: number,
+        gridSize: number,
+        left: number,
+        top: number,
+    ): fabric.Line[] {
         const lineOption = { stroke: "rgba(0,0,0,1)", strokeWidth: 1, selectable: false };
+        const lines = [];
+
         for (let i = Math.ceil(width / gridSize); i--; ) {
             lines.push(new fabric.Line([gridSize * i, -top, gridSize * i, height], lineOption));
         }
         for (let i = Math.ceil(height / gridSize); i--; ) {
             lines.push(new fabric.Line([-left, gridSize * i, width, gridSize * i], lineOption));
         }
+
+        return lines;
+    }
+
+    private addGridToCanvas(lines: fabric.Line[]): void {
         const oGridGroup = new fabric.Group(lines, {
             left: 0,
             top: 0,
