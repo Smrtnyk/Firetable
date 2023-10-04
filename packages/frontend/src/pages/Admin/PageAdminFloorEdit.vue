4166fdf82c87056ff033588f678edbed12255641
Smrtnyk <milankeser@outlook.de>
2023-10-04 20:55:26 +0200

    chore(PageAdminFloorEdit): fix floor reactivity tracking

--- packages/frontend/src/pages/Admin/PageAdminFloorEdit.vue
+++ packages/frontend/src/pages/Admin/PageAdminFloorEdit.vue
@@ -3,7 +3,7 @@ import AddTableDialog from "components/Floor/AddTableDialog.vue";
 import ShowSelectedElement from "components/Floor/ShowSelectedElement.vue";
 import FTDialog from "components/FTDialog.vue";
 
-import { nextTick, onMounted, ref, shallowRef } from "vue";
+import { nextTick, onMounted, ref } from "vue";
 import { NumberTuple } from "src/types/generic";
 import { useRouter } from "vue-router";
 import { Loading, useQuasar } from "quasar";
@@ -46,7 +46,7 @@ const addNewElementsBottomSheetOptions = {
 const props = defineProps<Props>();
 const router = useRouter();
 const q = useQuasar();
-const floorInstance = shallowRef<Floor | null>(null);
+const floorInstance = ref<Floor | null>(null);
 const canvasRef = ref<HTMLCanvasElement | null>(null);
 const pageRef = ref<HTMLDivElement | null>(null);
 const selectedElement = ref<FloorEditorElement | null>(null);
@@ -85,7 +85,7 @@ function instantiateFloor(floorDoc: FloorDoc) {
 }
 
 function onFloorSave(): void {
-    if (!floorInstance.value || !hasFloorTables(floorInstance.value)) {
+    if (!floorInstance.value || !hasFloorTables(floorInstance.value as Floor)) {
         return showErrorMessage("You need to add at least one table!");
     }
 
