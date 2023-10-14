c4da491c6beeac72cdd4bfb6abc96f1d776759e3
Smrtnyk <milankeser@outlook.de>
2023-10-14 13:24:41 +0200

    chore(useUsers): rewrite to not use vue fire

--- packages/frontend/src/composables/useUsers.ts
+++ packages/frontend/src/composables/useUsers.ts
@@ -1,7 +1,6 @@
 import { ref, watchEffect, computed } from "vue";
-import { documentId, where } from "firebase/firestore";
+import { query, where, getDocs, documentId } from "firebase/firestore";
 import { User, UserPropertyMapDoc } from "@firetable/types";
-import { createQuery, useFirestoreCollection } from "src/composables/useFirestore";
 import { useAuthStore } from "src/stores/auth-store";
 import { userPropertyMapCollection, usersCollection } from "@firetable/backend";
 
@@ -9,37 +8,28 @@ export function useUsers() {
     const authStore = useAuthStore();
     const users = ref<User[]>([]);
     const propertiesIds = computed(() => {
-        return (
-            authStore.userPropertyMap.map((map) => {
-                return map.propertyId;
-            }) || []
-        );
+        return authStore.userPropertyMap.map((map) => map.propertyId) || [];
     });
 
     watchEffect(async () => {
         if (propertiesIds.value.length > 0) {
-            const { data: allUsersIds, promise: userIdsPromise } =
-                useFirestoreCollection<UserPropertyMapDoc>(
-                    createQuery(
-                        userPropertyMapCollection(),
-                        where("propertyId", "in", propertiesIds.value),
-                    ),
-                    { once: true },
-                );
-            await userIdsPromise.value;
-            const userIdsToFetch = allUsersIds.value.map((map) => {
-                return map.userId;
-            });
-            const res = useFirestoreCollection<User>(
-                createQuery(
-                    usersCollection(),
-                    where(documentId(), "!=", authStore.user?.id),
-                    where(documentId(), "in", userIdsToFetch),
-                ),
-                { once: true },
+            const userPropertyMapQuery = query(
+                userPropertyMapCollection(),
+                where("propertyId", "in", propertiesIds.value),
             );
-            await res.promise.value;
-            users.value = res.data.value;
+            const userPropertyMapSnapshot = await getDocs(userPropertyMapQuery);
+            const allUsersIds = userPropertyMapSnapshot.docs.map(
+                (doc) => doc.data() as UserPropertyMapDoc,
+            );
+            const userIdsToFetch = allUsersIds.map((map) => map.userId);
+
+            const usersQuery = query(
+                usersCollection(),
+                where(documentId(), "!=", authStore.user?.id),
+                where(documentId(), "in", userIdsToFetch),
+            );
+            const usersSnapshot = await getDocs(usersQuery);
+            users.value = usersSnapshot.docs.map((doc) => doc.data() as User);
         } else {
             users.value = [];
         }
