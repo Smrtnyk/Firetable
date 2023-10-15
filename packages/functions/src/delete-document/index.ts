8c95cf589af84bc3f28d03069a6ea58d6160e827
Smrtnyk <milankeser@outlook.de>
2023-10-15 16:00:37 +0200

    chore(functions/delete document): add jsdoc

--- packages/functions/src/delete-document/index.ts
+++ packages/functions/src/delete-document/index.ts
@@ -1,6 +1,27 @@
 import { WriteResult } from "firebase-admin/firestore";
 import { db } from "../init.js";
 
+/**
+ * Deletes a specified document and all of its subcollections from Firestore.
+ *
+ * This function is designed to handle the deletion of a Firestore document along with any nested subcollections.
+ * Due to the Firestore data model, deleting a document does not automatically delete its subcollections.
+ * This function ensures that both the document and all its nested content are deleted.
+ *
+ * @param params - The parameters for the function.
+ * @param params.col - The name of the collection where the document resides.
+ * @param params.id - The ID of the document to delete.
+ *
+ * @throws - Throws an error if any part of the deletion process fails.
+ *
+ * @returns - A promise that resolves when the document and all its subcollections are deleted.
+ *
+ * @description
+ * The function first fetches all subcollections of the specified document.
+ * It then prepares to delete all documents within each subcollection.
+ * After all subcollections and their contents are deleted, the main document is deleted.
+ * If any deletion fails, the function throws an error with details.
+ */
 export async function deleteDocument({ col, id }: { col: string, id: string }): Promise<void> {
     const document = db.collection(col).doc(id);
 
