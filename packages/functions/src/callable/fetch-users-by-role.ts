68d04bfaa7280cd46e55667d0cc3bf2a94aaf67b
Smrtnyk <milankeser@outlook.de>
2023-10-15 15:55:36 +0200

    chore(functions/fetch users by role): refine and add jsdoc

--- packages/functions/src/callable/fetch-users-by-role.ts
+++ packages/functions/src/callable/fetch-users-by-role.ts
@@ -1,11 +1,39 @@
 import * as functions from "firebase-functions";
 import { db } from "../init.js";
-import { ADMIN, Collection, Role, User } from "../../types/types.js";
-import { FieldPath } from "firebase-admin/firestore";
+import { Role, Collection, User, ADMIN } from "../../types/types.js";
+import { FieldPath, Query } from "firebase-admin/firestore";
+
+type RoleFilter = {
+    [key in Role | typeof ADMIN | "default"]: (user: User) => boolean;
+};
 
 const MAX_USERS = 100;
 
-export async function fetchUsersByRoleFn(userIdsToFetch: string[], context: any): Promise<User[]> {
+/**
+ * Fetches users from the Firestore database based on the role of the authenticated user.
+ *
+ * - Admin users can fetch details of all users except themselves.
+ * - Property Owner users can fetch details of all users except Admins and themselves.
+ * - Manager users can fetch details of Managers and Staff, but not Property Owners, Admins, or themselves.
+ * - Staff users can only fetch details of other Staff users.
+ *
+ * This function also supports fetching a specific subset of users by passing their IDs, but Admins can ignore this and fetch all users.
+ *
+ * @param userIdsToFetch - An array of user IDs to fetch. For Admin users, this can be ignored.
+ * @param context - The context of the callable function, provided by Firebase Functions.
+ *                                                   This includes details about the authenticated user making the request.
+ *
+ * @throws - Throws an "unauthenticated" error if the user is not authenticated.
+ * @throws - Throws an "invalid-argument" error if too many user IDs are provided.
+ * @throws - Throws a "not-found" error if user role not found in custom claims.
+ *
+ * @returns - A promise that resolves to an array of User objects.
+ *
+ * @description
+ * The function uses the role of the authenticated user to determine which users it can fetch.
+ * It also applies a limit to the number of users that can be fetched to prevent overloading.
+ */
+export async function fetchUsersByRoleFn(userIdsToFetch: string[], context: functions.https.CallableContext): Promise<User[]> {
     functions.logger.log("fetchUsersByRoleFn called with userIds:", userIdsToFetch);
 
     // Ensure authentication
@@ -15,13 +43,12 @@ export async function fetchUsersByRoleFn(userIdsToFetch: string[], context: any)
     }
 
     const uid = context.auth.uid;
-    // Ensure the provided list of userIdsToFetch is not too large
     if (userIdsToFetch.length > MAX_USERS) {
         functions.logger.warn(`User ${uid} provided too many user IDs.`);
         throw new functions.https.HttpsError("invalid-argument", "Too many user IDs provided.");
     }
 
-    const userRole = context.auth.token.role;
+    const userRole = context.auth.token.role as Role | typeof ADMIN;
 
     if (!userRole) {
         functions.logger.error(`Role not found in custom claims for UID: ${uid}`);
@@ -30,41 +57,30 @@ export async function fetchUsersByRoleFn(userIdsToFetch: string[], context: any)
 
     functions.logger.log(`User ${uid} has role: ${userRole}`);
 
-    let baseQuery;
+    let baseQuery: Query = db.collection(Collection.USERS);
 
-    if (userRole === ADMIN) {
-        baseQuery = db.collection(Collection.USERS)
-            .where(FieldPath.documentId(), "!=", uid);
-    } else {
-        baseQuery = db.collection(Collection.USERS)
-            .where(FieldPath.documentId(), "in", userIdsToFetch)
-            .where(FieldPath.documentId(), "!=", uid);
+    if (userRole !== ADMIN) {
+        baseQuery = baseQuery.where(FieldPath.documentId(), "in", userIdsToFetch);
     }
+
+    baseQuery = baseQuery.where(FieldPath.documentId(), "!=", uid).limit(MAX_USERS);
 
     const snapshot = await baseQuery.get();
-    let users = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as User));
+    let users = snapshot.docs.map(function (doc) {
+        return ({ id: doc.id, ...doc.data() } as User);
+    });
 
-    switch (userRole) {
-        case ADMIN:
-            break;
-        case Role.PROPERTY_OWNER:
-            users = users.filter(u => u.role !== ADMIN);
-            break;
-        case Role.MANAGER:
-            users = users.filter(u => u.role !== ADMIN && u.role !== Role.PROPERTY_OWNER);
-            break;
-        default:
-            users = users.filter(u => u.role === Role.STAFF);
-            break;
-    }
+    const roleFilters: RoleFilter = {
+        [ADMIN]: () => true,
+        [Role.PROPERTY_OWNER]: (user: User) => user.role !== ADMIN,
+        [Role.MANAGER]: (user: User) => user.role !== ADMIN && user.role !== Role.PROPERTY_OWNER,
+        [Role.STAFF]: (user: User) => user.role === Role.STAFF,
+        default: (user: User) => user.role === Role.STAFF,
+    };
 
-    if (users.length > MAX_USERS) {
-        functions.logger.warn(`User ${uid} fetched too many users.`);
-        throw new functions.https.HttpsError("resource-exhausted", "Too many users matched the criteria.");
-    }
+    users = users.filter(roleFilters[userRole] || roleFilters.default);
 
     functions.logger.log(`Returning ${users.length} users for user ${uid}`);
 
     return users;
 }
-
