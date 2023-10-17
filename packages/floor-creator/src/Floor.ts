b7378e94f56fba8ca7709a77b16595c8813f721c
Smrtnyk <milankeser@outlook.de>
2023-10-17 22:08:12 +0200

    chore(Floor): add zoom and pan for mobile

--- packages/floor-creator/src/Floor.ts
+++ packages/floor-creator/src/Floor.ts
@@ -3,8 +3,6 @@ import {
     CANVAS_BG_COLOR,
     DEFAULT_COORDINATE,
     DEFAULT_ZOOM,
-    DOUBLE_TAP_DISTANCE_THRESHOLD,
-    DOUBLE_TAP_TIME_THRESHOLD,
     MAX_ZOOM_STEPS,
     RESOLUTION,
     ZOOM_INCREMENT,
@@ -49,8 +47,11 @@ export class Floor {
     width: number;
     private readonly initialScale: number;
     private initialViewportTransform: number[];
-    private lastTap: { timestamp: number; x: number; y: number } | null = null;
     elementManager: ElementManager;
+
+    private initialPinchDistance: number | null = null;
+    private initialDragX: number | null = null;
+    private initialDragY: number | null = null;
 
     constructor(options: FloorCreationOptions) {
         const { canvas, floorDoc, dblClickHandler, elementClickHandler, mode, containerWidth } =
@@ -86,8 +87,129 @@ export class Floor {
         this.initialViewportTransform = this.canvas.viewportTransform?.slice() || [];
         // @ts-ignore -- private prop
         const upperCanvasEl = this.canvas.upperCanvasEl;
-        upperCanvasEl.addEventListener("touchstart", this.nativeTouchHandler, { passive: true });
+        upperCanvasEl.addEventListener("touchstart", this.onTouchStart, { passive: true });
+        upperCanvasEl.addEventListener("touchmove", this.onTouchMove, { passive: true });
+        upperCanvasEl.addEventListener("touchend", this.onTouchEnd, { passive: true });
     }
+
+    private getDistance(touches: TouchList): number {
+        const [touch1, touch2] = [touches[0], touches[1]];
+        return Math.sqrt(
+            Math.pow(touch2.clientX - touch1.clientX, 2) +
+                Math.pow(touch2.clientY - touch1.clientY, 2),
+        );
+    }
+
+    private onTouchStart = (e: TouchEvent) => {
+        if (e.touches.length === 2) {
+            this.initialPinchDistance = this.getDistance(e.touches);
+        } else if (e.touches.length === 1) {
+            this.initialDragX = e.touches[0].clientX;
+            this.initialDragY = e.touches[0].clientY;
+        }
+    };
+
+    private onTouchMove = (e: TouchEvent) => {
+        const activeObject = this.canvas.getActiveObject();
+
+        // If an object is selected, don't pan the canvas
+        if (activeObject) {
+            return;
+        }
+        // Handle pinch to zoom
+        if (e.touches.length === 2 && this.initialPinchDistance !== null) {
+            const newDistance = this.getDistance(e.touches);
+            const scale = newDistance / this.initialPinchDistance;
+
+            const delta = scale > 1 ? 10 : -10; // Adjust these numbers to modify sensitivity
+
+            this.handleZoomLogic({
+                e: { deltaY: delta, offsetX: e.touches[0].clientX, offsetY: e.touches[0].clientY },
+            } as fabric.IEvent<WheelEvent>);
+
+            // Update the initial distance for the next move.
+            this.initialPinchDistance = newDistance;
+        }
+        // Handle single touch move for panning
+        else if (
+            e.touches.length === 1 &&
+            this.initialDragX != undefined &&
+            this.initialDragY != undefined
+        ) {
+            if (this.canvas.getZoom() === this.initialScale) {
+                return; // No panning if not zoomed in
+            }
+
+            const deltaX = e.touches[0].clientX - this.initialDragX;
+            const deltaY = e.touches[0].clientY - this.initialDragY;
+
+            const { newPosX, newPosY } = this.checkBoundaries(deltaX, deltaY);
+
+            const viewportTransform = this.canvas.viewportTransform?.slice() || [1, 0, 0, 1, 0, 0];
+            viewportTransform[4] = newPosX;
+            viewportTransform[5] = newPosY;
+
+            this.canvas.setViewportTransform(viewportTransform);
+            this.canvas.requestRenderAll();
+
+            this.initialDragX = e.touches[0].clientX;
+            this.initialDragY = e.touches[0].clientY;
+        }
+    };
+
+    private onTouchEnd = () => {
+        this.initialPinchDistance = null;
+    };
+
+    checkBoundaries(deltaX: number, deltaY: number) {
+        let newPosX =
+            (this.canvas.viewportTransform ? this.canvas.viewportTransform[4] : 0) + deltaX;
+        let newPosY =
+            (this.canvas.viewportTransform ? this.canvas.viewportTransform[5] : 0) + deltaY;
+
+        // Check boundaries for X
+        if (newPosX > 0) {
+            newPosX = 0;
+        } else if (newPosX + this.width * this.canvas.getZoom() < this.canvas.getWidth()) {
+            newPosX = this.canvas.getWidth() - this.width * this.canvas.getZoom();
+        }
+
+        // Check boundaries for Y
+        if (newPosY > 0) {
+            newPosY = 0;
+        } else if (newPosY + this.height * this.canvas.getZoom() < this.canvas.getHeight()) {
+            newPosY = this.canvas.getHeight() - this.height * this.canvas.getZoom();
+        }
+
+        return { newPosX, newPosY };
+    }
+
+    private handleZoomLogic = (opt: fabric.IEvent<WheelEvent>) => {
+        if (!opt.e) {
+            return;
+        }
+
+        const delta = opt.e.deltaY;
+        let shouldReset = false;
+
+        if (delta > 0 && this.currentZoomSteps < MAX_ZOOM_STEPS) {
+            this.currentZoomSteps++;
+            this.performZoom(opt.e.offsetX, opt.e.offsetY);
+        } else if (delta < 0 && this.currentZoomSteps > 0) {
+            this.currentZoomSteps--;
+            this.performZoom(opt.e.offsetX, opt.e.offsetY);
+            if (this.currentZoomSteps <= 0) {
+                shouldReset = true;
+            }
+        }
+
+        if (shouldReset) {
+            this.resetToInitialState();
+        }
+
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+    };
 
     get json() {
         return this.canvas.toJSON(["label", "reservation", "name", "type"]);
@@ -112,43 +234,6 @@ export class Floor {
             this.elementClickHandler(this, e.target);
         });
     }
-
-    private handleDoubleTap(x: number, y: number, timestamp: number) {
-        if (!this.lastTap) return;
-
-        const timeDifference = timestamp - this.lastTap.timestamp;
-        const distance = Math.sqrt(
-            Math.pow(x - this.lastTap.x, 2) + Math.pow(y - this.lastTap.y, 2),
-        );
-
-        if (
-            timeDifference < DOUBLE_TAP_TIME_THRESHOLD &&
-            distance < DOUBLE_TAP_DISTANCE_THRESHOLD
-        ) {
-            // This is considered a double tap
-            this.dblClickHandler?.(this, [x, y]);
-            this.lastTap = null;
-        } else {
-            // This is a single tap or the start of a double-tap
-            this.lastTap = { timestamp, x, y };
-        }
-    }
-
-    private nativeTouchHandler = (e: TouchEvent) => {
-        e.preventDefault();
-
-        const timestamp = new Date().getTime();
-        const touch = e.changedTouches[0];
-        let x = touch.clientX;
-        let y = touch.clientY;
-
-        const rect = (e.target as HTMLCanvasElement).getBoundingClientRect();
-        const scale = this.scale;
-        x = (x - rect.left) / scale;
-        y = (y - rect.top) / scale;
-
-        this.handleDoubleTap(x, y, timestamp);
-    };
 
     private onMouseWheelHandler = (opt: fabric.IEvent<WheelEvent>) => {
         if (!opt.e) {
