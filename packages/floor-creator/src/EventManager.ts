e1c0ac9f9221cb85cc1456968ac24b955a7bf8f1
Smrtnyk <milankeser@outlook.de>
2023-10-18 00:09:57 +0200

    chore(Floor): add EventManager

---
+++ packages/floor-creator/src/EventManager.ts
@@ -0,0 +1,84 @@
+import { fabric } from "fabric";
+import { Floor } from "./Floor";
+import { NumberTuple } from "./types";
+import { isFloorElement, isTable } from "./type-guards";
+import { DEFAULT_COORDINATE, RESOLUTION } from "./constants";
+
+export class EventManager {
+    private readonly floor: Floor;
+
+    constructor(floor: Floor) {
+        this.floor = floor;
+    }
+
+    initializeCanvasEventHandlers() {
+        this.floor.canvas.on("mouse:dblclick", this.onDblClickHandler);
+        this.floor.canvas.on("object:moving", this.onObjectMove);
+        this.floor.canvas.on("mouse:wheel", this.onMouseWheelHandler);
+        this.floor.canvas.on("object:scaling", this.onObjectScaling);
+
+        // @ts-ignore -- private prop
+        const upperCanvasEl = this.floor.canvas.upperCanvasEl as HTMLElement;
+        upperCanvasEl.addEventListener("touchstart", this.floor.touchManager.onTouchStart, {
+            passive: true,
+        });
+        upperCanvasEl.addEventListener("touchmove", this.floor.touchManager.onTouchMove, {
+            passive: true,
+        });
+        upperCanvasEl.addEventListener("touchend", this.floor.touchManager.onTouchEnd, {
+            passive: true,
+        });
+    }
+
+    onObjectMove = (options: fabric.IEvent) => {
+        if (!options.target?.left || !options.target?.top) return;
+        const shouldSnapToGrid =
+            Math.round((options.target.left / RESOLUTION) * 4) % 4 === 0 &&
+            Math.round((options.target.top / RESOLUTION) * 4) % 4 === 0;
+        if (shouldSnapToGrid) {
+            options.target
+                .set({
+                    left: Math.round(options.target.left / RESOLUTION) * RESOLUTION,
+                    top: Math.round(options.target.top / RESOLUTION) * RESOLUTION,
+                })
+                .setCoords();
+        }
+    };
+
+    // Check if double click was on the actual table
+    // if it is, then do nothing, but if it is not
+    // then invoke the handler
+    private onDblClickHandler = (ev: fabric.IEvent) => {
+        if (isFloorElement(ev.target)) return;
+        const coords: NumberTuple = [
+            ev.pointer?.x || DEFAULT_COORDINATE,
+            ev.pointer?.y || DEFAULT_COORDINATE,
+        ];
+        this.floor.dblClickHandler?.(this.floor, coords);
+    };
+
+    private onMouseWheelHandler = (opt: fabric.IEvent<WheelEvent>) => {
+        if (!opt.e) {
+            return;
+        }
+
+        const delta = opt.e.deltaY;
+
+        if (delta > 0 && this.floor.zoomManager.canZoomIn()) {
+            this.floor.zoomManager.zoomIn(new fabric.Point(opt.e.offsetX, opt.e.offsetY));
+        } else if (delta < 0 && this.floor.zoomManager.canZoomOut()) {
+            this.floor.zoomManager.zoomOut(new fabric.Point(opt.e.offsetX, opt.e.offsetY));
+            if (!this.floor.zoomManager.canZoomOut()) {
+                this.floor.zoomManager.resetZoom();
+            }
+        }
+
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+    };
+
+    private onObjectScaling = (e: fabric.IEvent) => {
+        if (!isTable(e.target)) return;
+        this.floor.elementClickHandler(this.floor, e.target);
+    };
+}
--- packages/floor-creator/src/Floor.ts
+++ packages/floor-creator/src/Floor.ts
@@ -1,21 +1,20 @@
 import { fabric } from "fabric";
-import { CANVAS_BG_COLOR, DEFAULT_COORDINATE, RESOLUTION } from "./constants.js";
+import { CANVAS_BG_COLOR } from "./constants.js";
 import {
     BaseTable,
     CreateElementOptions,
     ElementClickHandler,
     FloorDoubleClickHandler,
     FloorMode,
-    NumberTuple,
 } from "./types.js";
 import { FloorDoc, Reservation } from "@firetable/types";
-import { isFloorElement, isTable } from "./type-guards";
 import { RoundTable } from "./elements/RoundTable";
 import { RectTable } from "./elements/RectTable";
 import { ElementManager } from "./ElementManager";
 import { TouchManager } from "./TouchManager";
 import { GridDrawer } from "./GridDrawer";
 import { FloorZoomManager } from "./FloorZoomManager";
+import { EventManager } from "./EventManager.js";
 
 interface FloorCreationOptions {
     canvas: HTMLCanvasElement;
@@ -38,15 +37,15 @@ export class Floor {
     readonly canvas: fabric.Canvas;
     readonly dblClickHandler?: FloorDoubleClickHandler;
     readonly elementClickHandler: ElementClickHandler;
-    currentZoomSteps = 0;
     mode: FloorMode;
     width: number;
     readonly initialScale: number;
     private readonly initialViewportTransform: number[];
     elementManager: ElementManager;
-    private touchManager: TouchManager;
+    touchManager: TouchManager;
     private gridDrawer: GridDrawer;
     zoomManager: FloorZoomManager;
+    eventManager: EventManager;
 
     constructor(options: FloorCreationOptions) {
         const { canvas, floorDoc, dblClickHandler, elementClickHandler, mode, containerWidth } =
@@ -88,7 +87,9 @@ export class Floor {
         );
 
         this.touchManager = new TouchManager(this);
-        this.initializeCanvasEventHandlers();
+
+        this.eventManager = new EventManager(this);
+        this.eventManager.initializeCanvasEventHandlers();
     }
 
     get json() {
@@ -101,88 +102,9 @@ export class Floor {
         this.canvas.add(element);
     }
 
-    zoomIn(point: fabric.Point) {
-        this.zoomManager.zoomIn(point);
-    }
-
-    zoomOut(point: fabric.Point) {
-        this.zoomManager.zoomOut(point);
-    }
-
     private get isInEditorMode(): boolean {
         return this.mode === FloorMode.EDITOR;
     }
-
-    private initializeCanvasEventHandlers() {
-        this.registerDoubleClickHandler();
-        this.registerObjectMoveHandler();
-        this.registerMouseWheelHandler();
-        this.registerObjectScalingHandler();
-        this.registerTouchHandlers();
-    }
-
-    private registerDoubleClickHandler() {
-        this.canvas.on("mouse:dblclick", this.onDblClickHandler);
-    }
-
-    private registerObjectMoveHandler() {
-        this.canvas.on("object:moving", this.onObjectMove);
-    }
-
-    private registerMouseWheelHandler() {
-        this.canvas.on("mouse:wheel", this.onMouseWheelHandler);
-    }
-
-    private registerObjectScalingHandler() {
-        this.canvas.on("object:scaling", (e) => {
-            if (!isTable(e.target)) return;
-            this.elementClickHandler(this, e.target);
-        });
-    }
-
-    private registerTouchHandlers() {
-        // @ts-ignore -- private prop
-        const upperCanvasEl = this.canvas.upperCanvasEl;
-        upperCanvasEl.addEventListener("touchstart", this.touchManager.onTouchStart, {
-            passive: true,
-        });
-        upperCanvasEl.addEventListener("touchmove", this.touchManager.onTouchMove, {
-            passive: true,
-        });
-        upperCanvasEl.addEventListener("touchend", this.touchManager.onTouchEnd, { passive: true });
-    }
-
-    private onMouseWheelHandler = (opt: fabric.IEvent<WheelEvent>) => {
-        if (!opt.e) {
-            return;
-        }
-
-        const delta = opt.e.deltaY;
-
-        if (delta > 0 && this.zoomManager.canZoomIn()) {
-            this.zoomManager.zoomIn(new fabric.Point(opt.e.offsetX, opt.e.offsetY));
-        } else if (delta < 0 && this.zoomManager.canZoomOut()) {
-            this.zoomManager.zoomOut(new fabric.Point(opt.e.offsetX, opt.e.offsetY));
-            if (!this.zoomManager.canZoomOut()) {
-                this.zoomManager.resetZoom();
-            }
-        }
-
-        opt.e.preventDefault();
-        opt.e.stopPropagation();
-    };
-
-    // Check if double click was on the actual table
-    // if it is, then do nothing, but if it is not
-    // then invoke the handler
-    private onDblClickHandler = (ev: fabric.IEvent) => {
-        if (isFloorElement(ev.target)) return;
-        const coords: NumberTuple = [
-            ev.pointer?.x || DEFAULT_COORDINATE,
-            ev.pointer?.y || DEFAULT_COORDINATE,
-        ];
-        this.dblClickHandler?.(this, coords);
-    };
 
     onElementClick = (ev: fabric.IEvent<MouseEvent>) => {
         this.elementClickHandler(this, ev.target);
@@ -192,21 +114,6 @@ export class Floor {
         object.on("mouseup", this.onElementClick);
         object.lockMovementY = this.shouldLockDrag();
         object.lockMovementX = this.shouldLockDrag();
-    };
-
-    onObjectMove = (options: fabric.IEvent) => {
-        if (!options.target?.left || !options.target?.top) return;
-        const shouldSnapToGrid =
-            Math.round((options.target.left / RESOLUTION) * 4) % 4 === 0 &&
-            Math.round((options.target.top / RESOLUTION) * 4) % 4 === 0;
-        if (shouldSnapToGrid) {
-            options.target
-                .set({
-                    left: Math.round(options.target.left / RESOLUTION) * RESOLUTION,
-                    top: Math.round(options.target.top / RESOLUTION) * RESOLUTION,
-                })
-                .setCoords();
-        }
     };
 
     setScaling() {
