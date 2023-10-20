eb0d614d8a4b49b1ebb6ae824a7d79c5bf59f068
Smrtnyk <milankeser@outlook.de>
2023-10-20 16:56:23 +0200

    chore(Floor): improve sofa design

--- packages/floor-creator/src/elements/SingleSofa.ts
+++ packages/floor-creator/src/elements/SingleSofa.ts
@@ -1,68 +1,29 @@
 import { fabric } from "fabric";
+import { RESOLUTION } from "../constants";
 
 export class SingleSofa extends fabric.Group {
     constructor(left: number, top: number) {
         const base = new fabric.Rect({
             left: 0,
             top: 0,
-            rx: 10,
-            ry: 10,
-            width: 60, // Width is reduced for single seater
-            height: 30,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
+            width: RESOLUTION * 2,
+            height: RESOLUTION / 2,
+            fill: "#444",
+            stroke: "#222",
+            strokeWidth: 0.5,
         });
 
         const backrest = new fabric.Rect({
             left: 0,
-            top: -20,
-            rx: 10,
-            ry: 10,
-            width: 60,
-            height: 20,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
+            top: -RESOLUTION / 4,
+            width: RESOLUTION * 2,
+            height: RESOLUTION / 4,
+            fill: "#444",
+            stroke: "#222",
+            strokeWidth: 0.5,
         });
 
-        const armrestLeft = new fabric.Rect({
-            left: -10,
-            top: 0,
-            rx: 10,
-            ry: 10,
-            width: 20,
-            height: 35,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        const armrestRight = new fabric.Rect({
-            left: 50, // Adjusted for the reduced width
-            top: 0,
-            rx: 10,
-            ry: 10,
-            width: 20,
-            height: 35,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        const cushion = new fabric.Rect({
-            left: 10, // Centered the cushion for single-seater
-            top: 0,
-            rx: 5,
-            ry: 5,
-            width: 40, // Cushion width adjusted for single-seater
-            height: 35,
-            fill: "#A0522D",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        super([base, backrest, armrestLeft, armrestRight, cushion], { left, top });
+        super([base, backrest], { left, top });
     }
 }
 
--- packages/floor-creator/src/elements/Sofa.ts
+++ packages/floor-creator/src/elements/Sofa.ts
@@ -1,80 +1,29 @@
 import { fabric } from "fabric";
+import { RESOLUTION } from "../constants";
 
 export class Sofa extends fabric.Group {
     constructor(left: number, top: number) {
         const base = new fabric.Rect({
             left: 0,
             top: 0,
-            rx: 10,
-            ry: 10,
-            width: 100,
-            height: 30,
-            fill: "#8B4513", // SaddleBrown
-            stroke: "black",
-            strokeWidth: 1,
+            width: RESOLUTION * 4,
+            height: RESOLUTION / 2,
+            fill: "#444",
+            stroke: "#222",
+            strokeWidth: 0.5,
         });
 
         const backrest = new fabric.Rect({
             left: 0,
-            top: -20,
-            rx: 10,
-            ry: 10,
-            width: 100,
-            height: 20,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
+            top: -RESOLUTION / 4,
+            width: RESOLUTION * 4,
+            height: RESOLUTION / 4,
+            fill: "#444",
+            stroke: "#222",
+            strokeWidth: 0.5,
         });
 
-        const armrestLeft = new fabric.Rect({
-            left: -10,
-            top: 0,
-            rx: 10,
-            ry: 10,
-            width: 20,
-            height: 35,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        const armrestRight = new fabric.Rect({
-            left: 90,
-            top: 0,
-            rx: 10,
-            ry: 10,
-            width: 20,
-            height: 35,
-            fill: "#8B4513",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        const cushion1 = new fabric.Rect({
-            left: 15,
-            top: 0,
-            rx: 5,
-            ry: 5,
-            width: 35,
-            height: 35,
-            fill: "#A0522D", // Sienna
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        const cushion2 = new fabric.Rect({
-            left: 50,
-            top: 0,
-            rx: 5,
-            ry: 5,
-            width: 35,
-            height: 35,
-            fill: "#A0522D",
-            stroke: "black",
-            strokeWidth: 1,
-        });
-
-        super([base, backrest, armrestLeft, armrestRight, cushion1, cushion2], { left, top });
+        super([base, backrest], { left, top });
     }
 }
 
