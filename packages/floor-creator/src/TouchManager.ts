d8bb61069187d3da854352a77c96b9536f1c6ae1
Smrtnyk <milankeser@outlook.de>
2023-10-20 14:08:50 +0200

    chore(TouchManager): momentum based panning

--- packages/floor-creator/src/TouchManager.ts
+++ packages/floor-creator/src/TouchManager.ts
@@ -6,6 +6,15 @@ export class TouchManager {
     private initialDragY?: number;
     private isPinching: boolean = false;
 
+    // For momentum-based panning
+    private lastMoveTimestamp?: number;
+    private lastMoveX?: number;
+    private lastMoveY?: number;
+    private velocityX: number = 0;
+    private velocityY: number = 0;
+    private friction: number = 0.95; // Friction factor to reduce velocity
+    private animationFrame?: number;
+
     constructor(floor: Floor) {
         this.floor = floor;
     }
@@ -20,6 +29,13 @@ export class TouchManager {
             this.initialDragX = e.touches[0].clientX;
             this.initialDragY = e.touches[0].clientY;
         }
+
+        // Reset momentum panning tracking values
+        this.lastMoveTimestamp = undefined;
+        this.lastMoveX = undefined;
+        this.lastMoveY = undefined;
+        this.velocityX = 0;
+        this.velocityY = 0;
     };
 
     onTouchMove = (e: TouchEvent) => {
@@ -39,6 +55,17 @@ export class TouchManager {
         ) {
             this.handlePanning(e);
         }
+
+        // For momentum-based panning
+        const currentTimestamp = performance.now();
+        if (this.lastMoveTimestamp && this.lastMoveX && this.lastMoveY) {
+            const deltaTime = currentTimestamp - this.lastMoveTimestamp;
+            this.velocityX = (e.touches[0].clientX - this.lastMoveX) / deltaTime;
+            this.velocityY = (e.touches[0].clientY - this.lastMoveY) / deltaTime;
+        }
+        this.lastMoveTimestamp = currentTimestamp;
+        this.lastMoveX = e.touches[0].clientX;
+        this.lastMoveY = e.touches[0].clientY;
     };
 
     private handlePanning(e: TouchEvent) {
@@ -63,7 +90,67 @@ export class TouchManager {
     onTouchEnd = () => {
         this.isPinching = false;
         this.floor.zoomManager.initialPinchDistance = undefined;
+
+        // Start momentum-based panning
+        if (this.animationFrame) {
+            cancelAnimationFrame(this.animationFrame);
+        }
+        this.animateMomentumPanning();
     };
+
+    private animateMomentumPanning() {
+        if (Math.abs(this.velocityX) > 0.01 || Math.abs(this.velocityY) > 0.01) {
+            // Calculate the new position without clamping
+            let newPosX =
+                (this.floor.canvas.viewportTransform ? this.floor.canvas.viewportTransform[4] : 0) +
+                this.velocityX;
+            let newPosY =
+                (this.floor.canvas.viewportTransform ? this.floor.canvas.viewportTransform[5] : 0) +
+                this.velocityY;
+
+            // Check boundaries for X
+            if (newPosX > 0) {
+                newPosX = 0;
+                this.velocityX = 0; // Stop motion in X direction
+            } else if (
+                newPosX + this.floor.width * this.floor.canvas.getZoom() <
+                this.floor.canvas.getWidth()
+            ) {
+                newPosX =
+                    this.floor.canvas.getWidth() - this.floor.width * this.floor.canvas.getZoom();
+                this.velocityX = 0; // Stop motion in X direction
+            }
+
+            // Check boundaries for Y
+            if (newPosY > 0) {
+                newPosY = 0;
+                this.velocityY = 0; // Stop motion in Y direction
+            } else if (
+                newPosY + this.floor.height * this.floor.canvas.getZoom() <
+                this.floor.canvas.getHeight()
+            ) {
+                newPosY =
+                    this.floor.canvas.getHeight() - this.floor.height * this.floor.canvas.getZoom();
+                this.velocityY = 0; // Stop motion in Y direction
+            }
+
+            const viewportTransform = this.floor.canvas.viewportTransform?.slice() || [
+                1, 0, 0, 1, 0, 0,
+            ];
+            viewportTransform[4] = newPosX;
+            viewportTransform[5] = newPosY;
+
+            this.floor.canvas.setViewportTransform(viewportTransform);
+            this.floor.canvas.requestRenderAll();
+
+            this.velocityX *= this.friction;
+            this.velocityY *= this.friction;
+
+            this.animationFrame = requestAnimationFrame(this.animateMomentumPanning.bind(this));
+        } else {
+            this.floor.canvas.requestRenderAll();
+        }
+    }
 
     private checkBoundaries(deltaX: number, deltaY: number) {
         let newPosX =
