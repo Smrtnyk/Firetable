ad3357ea6007df147ed86cc70948edee6a5d997b
Smrtnyk <milankeser@outlook.de>
2023-10-25 20:56:37 +0200

    chore(FTAutocomplete): add autocomplete

--- packages/frontend/src/components/Event/FTAutocomplete.vue
+++ packages/frontend/src/components/Event/FTAutocomplete.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="FTAutocomplete">
-        <q-input
+        <q-select
+            fill-input
+            hide-selected
+            use-input
             v-model="searchTerm"
             :label="t(`FTAutocomplete.label`)"
             clearable
@@ -8,14 +11,22 @@
             dense
             standout
             rounded
-            :debounce="500"
-            @update:model-value="onTablesSearch"
-            @clearlear="() => emit('clear')"
+            :debounce="300"
+            @input-value="setModel"
+            @filter="filterFn"
+            @clear="() => emit('clear')"
+            :options="options"
         >
             <template #prepend>
                 <q-icon name="search" />
             </template>
-        </q-input>
+
+            <template #no-option>
+                <q-item>
+                    <q-item-section class="text-grey"> No results </q-item-section>
+                </q-item>
+            </template>
+        </q-select>
     </div>
 </template>
 
@@ -31,26 +42,43 @@ interface Props {
 
 const props = defineProps<Props>();
 const emit = defineEmits(["found", "clear"]);
+const options = ref(tablesToNames(props.allReservedTables));
 
 const searchTerm = ref("");
 const { t } = useI18n();
 
-function onTablesSearch(val: string | number | null) {
-    if (!val || isNumber(val)) {
-        emit("clear");
-        return;
-    }
-
-    const tokens = val.toLowerCase().split(/\s+/);
-
-    const found = props.allReservedTables.filter((table) => {
-        if (!table.reservation) return false;
-        const normalizedGuestName = table.reservation.guestName.toLowerCase();
-        const guestNameTokens = normalizedGuestName.split(/\s+/);
-
-        return tokens.every((token) => guestNameTokens.includes(token));
+function tablesToNames(tables: BaseTable[]) {
+    return tables.map((table) => {
+        return table.reservation!.guestName;
     });
+}
 
-    emit("found", found);
+function filterFn(val: string, update: any) {
+    update(() => {
+        if (!val || isNumber(val)) {
+            options.value = tablesToNames(props.allReservedTables);
+            emit("clear");
+            return;
+        }
+
+        const tokens = val.toLowerCase().split(/\s+/);
+
+        const found = props.allReservedTables.filter((table) => {
+            const guestName = table.reservation!.guestName;
+            const normalizedGuestName = guestName.toLowerCase();
+            const guestNameTokens = normalizedGuestName.split(/\s+/);
+
+            return tokens.some((token) =>
+                guestNameTokens.some((nameToken) => nameToken.includes(token)),
+            );
+        });
+
+        options.value = tablesToNames(found);
+        emit("found", found);
+    });
+}
+
+function setModel(val: string) {
+    searchTerm.value = val;
 }
 </script>
