0d33431f4cb651a4654d19f6ac50bc293bfe4d08
Smrtnyk <milankeser@outlook.de>
2023-10-26 21:05:50 +0200

    chore(FTAutocomplete): make sure to react on click from list of items

--- packages/frontend/src/components/Event/FTAutocomplete.vue
+++ packages/frontend/src/components/Event/FTAutocomplete.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { BaseTable } from "@firetable/floor-creator";
 import { isNumber } from "@firetable/utils";
@@ -42,41 +42,44 @@ interface Props {
 
 const props = defineProps<Props>();
 const emit = defineEmits(["found", "clear"]);
-const options = ref(tablesToNames(props.allReservedTables));
-
-const searchTerm = ref("");
 const { t } = useI18n();
 
-function tablesToNames(tables: BaseTable[]) {
-    return tables.map((table) => {
-        return table.reservation!.guestName;
+const options = ref(getNamesFromTables(props.allReservedTables));
+const searchTerm = ref("");
+
+function getNamesFromTables(tables: BaseTable[]) {
+    return tables.map((table) => table.reservation!.guestName);
+}
+
+function findSearchedTable(val: string) {
+    const tokens = val.toLowerCase().split(/\s+/);
+
+    return props.allReservedTables.filter((table) => {
+        const { guestName } = table.reservation!;
+        const normalizedGuestName = guestName.toLowerCase();
+        const guestNameTokens = normalizedGuestName.split(/\s+/);
+
+        return tokens.some((token) =>
+            guestNameTokens.some((nameToken) => nameToken.includes(token)),
+        );
     });
 }
 
 function filterFn(val: string, update: any) {
     update(() => {
-        if (!val || isNumber(val)) {
-            options.value = tablesToNames(props.allReservedTables);
-            emit("clear");
-            return;
-        }
-
-        const tokens = val.toLowerCase().split(/\s+/);
-
-        const found = props.allReservedTables.filter((table) => {
-            const guestName = table.reservation!.guestName;
-            const normalizedGuestName = guestName.toLowerCase();
-            const guestNameTokens = normalizedGuestName.split(/\s+/);
-
-            return tokens.some((token) =>
-                guestNameTokens.some((nameToken) => nameToken.includes(token)),
-            );
-        });
-
-        options.value = tablesToNames(found);
-        emit("found", found);
+        options.value =
+            !val || isNumber(val)
+                ? getNamesFromTables(props.allReservedTables)
+                : getNamesFromTables(findSearchedTable(val));
     });
 }
+
+watch(searchTerm, (newTerm) => {
+    if (newTerm) {
+        const found = findSearchedTable(newTerm);
+        emit("found", found);
+    }
+});
 
 function setModel(val: string) {
     searchTerm.value = val;
