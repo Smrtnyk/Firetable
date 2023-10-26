57568f3fef601a5f076a96ea3482c4ad0d7b2570
Smrtnyk <milankeser@outlook.de>
2023-10-26 18:59:42 +0200

    feat(ShowSelectedElement): add color picker feature so that elements can be colored

--- packages/frontend/src/components/Floor/ShowSelectedElement.vue
+++ packages/frontend/src/components/Floor/ShowSelectedElement.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row q-pa-sm q-col-gutter-md" v-if="selectedElement">
-        <div class="col-10 flex justify-between">
+        <div class="col-8 flex justify-between">
             <div class="row">
                 <div class="col-4 q-pa-xs q-pl-none">
                     <q-input
@@ -32,7 +32,27 @@
                 </div>
             </div>
         </div>
-        <div class="col-2 flex q-pl-none justify-end">
+        <div class="col-4 flex q-pl-none justify-end">
+            <!-- Color Picker Button -->
+            <q-btn
+                v-if="elementColor"
+                :style="{ 'background-color': elementColor }"
+                @click="openColorPicker"
+                :size="buttonSize"
+            >
+                <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                <q-popup-proxy
+                    no-parent-event
+                    ref="colorPickerProxy"
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                >
+                    <q-color :model-value="elementColor" @update:model-value="setElementColor" />
+                </q-popup-proxy>
+            </q-btn>
+
+            <!-- Delete Button -->
             <q-btn
                 v-if="selectedElement && props.deleteAllowed"
                 icon="trash"
@@ -58,18 +78,19 @@
 
 <script setup lang="ts">
 import { showConfirm, showErrorMessage } from "src/helpers/ui-helpers";
-import { computed, nextTick } from "vue";
+import { computed, nextTick, ref, watch } from "vue";
 import { FloorEditorElement, isTable } from "@firetable/floor-creator";
-import { useQuasar } from "quasar";
-
-const q = useQuasar();
-const isMobile = computed(() => q.screen.lt.sm);
-const buttonSize = computed(() => (isMobile.value ? "xs" : "md"));
+import { QPopupProxy, useQuasar } from "quasar";
 
 interface Props {
     selectedFloorElement: FloorEditorElement | undefined;
     deleteAllowed?: boolean;
 }
+
+const q = useQuasar();
+const isMobile = computed(() => q.screen.lt.sm);
+const buttonSize = computed(() => (isMobile.value ? "xs" : "md"));
+const colorPickerProxy = ref<QPopupProxy | null>(null);
 
 const props = withDefaults(defineProps<Props>(), {
     deleteAllowed: true,
@@ -78,6 +99,20 @@ const emit = defineEmits(["delete"]);
 const selectedElement = computed(() => {
     return props.selectedFloorElement;
 });
+const elementColor = ref<string>("");
+
+watch(
+    () => props.selectedFloorElement,
+    () => {
+        if (props.selectedFloorElement) {
+            elementColor.value = props.selectedFloorElement!.getBaseFill?.() || "";
+        }
+    },
+);
+
+function openColorPicker() {
+    colorPickerProxy.value?.show();
+}
 
 async function updateTableLabel(newId: string | number | null): Promise<void> {
     if (!selectedElement.value || !newId) return;
@@ -107,5 +142,10 @@ function getElementWidth(): number {
 function getElementHeight(): number {
     const el = selectedElement.value!;
     return Math.round(el.height! * el.scaleY!);
+}
+
+function setElementColor(newVal: any) {
+    elementColor.value = newVal;
+    props.selectedFloorElement?.setBaseFill?.(newVal);
 }
 </script>
