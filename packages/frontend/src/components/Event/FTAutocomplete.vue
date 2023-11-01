ad91ee9e2fb223b8963cdaf15355dcbd18ea9086
Smrtnyk <milankeser@outlook.de>
2023-11-01 20:15:23 +0100

    FTAutocomplete.vue: improve options to also show table label

--- packages/frontend/src/components/Event/FTAutocomplete.vue
+++ packages/frontend/src/components/Event/FTAutocomplete.vue
@@ -34,7 +34,6 @@
 import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { BaseTable } from "@firetable/floor-creator";
-import { isNumber } from "@firetable/utils";
 
 interface Props {
     allReservedTables: BaseTable[];
@@ -48,10 +47,14 @@ const options = ref(getNamesFromTables(props.allReservedTables));
 const searchTerm = ref("");
 
 function getNamesFromTables(tables: BaseTable[]) {
-    return tables.map((table) => table.reservation!.guestName);
+    return tables.map((table) => ({
+        label: `${table.reservation!.guestName} (${table.label})`,
+        value: table.reservation!.guestName,
+    }));
 }
 
-function findSearchedTable(val: string) {
+function findSearchedTable(inputVal: string | { value: string }) {
+    const val = typeof inputVal === "string" ? inputVal : inputVal.value;
     const tokens = val.toLowerCase().split(/\s+/);
 
     return props.allReservedTables.filter((table) => {
@@ -67,10 +70,14 @@ function findSearchedTable(val: string) {
 
 function filterFn(val: string, update: any) {
     update(() => {
-        options.value =
-            !val || isNumber(val)
-                ? getNamesFromTables(props.allReservedTables)
-                : getNamesFromTables(findSearchedTable(val));
+        const loweredVal = val.toLowerCase();
+        const filteredTables = findSearchedTable(val);
+        options.value = filteredTables
+            .map((table) => ({
+                label: `${table.reservation!.guestName} (${table.label})`,
+                value: table.reservation!.guestName,
+            }))
+            .filter((option) => option.value.toLowerCase().includes(loweredVal));
     });
 }
 
