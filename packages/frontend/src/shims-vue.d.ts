6a694f7fcbc918ba7804c39a8adf975f3d3cbe8d
Smrtnyk <milankeser@outlook.de>
2023-11-04 12:57:28 +0100

    chore: d.ts files cleanup

--- packages/frontend/src/shims-vue.d.ts
+++ packages/frontend/src/shims-vue.d.ts
@@ -4,8 +4,8 @@
 /// <reference types="vue/ref-macros" />
 
 // Mocks all files ending in `.vue` showing them as plain Vue instances
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
+declare module "*.vue" {
+    import type { DefineComponent } from "vue";
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
 }
