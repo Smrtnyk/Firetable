525a6d6e5ea900757c7228aca8d35e88db0959fb
Smrtnyk <milankeser@outlook.de>
2023-11-05 19:40:18 +0100

    utils: remove redundant code

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,6 @@
-export { matchesValue } from "./matches-value";
 export { memoize } from "./memoize";
 export { not } from "./not";
 export { propIsTruthy } from "./prop-is-truthy";
-export { range } from "./range";
 export { takeLast } from "./take-last";
 export { takeProp } from "./take-prop";
 export * from "./type-guards";
--- packages/utils/src/matches-value.ts
+++
@@ -1,5 +0,0 @@
-export function matchesValue(value: unknown) {
-    return function (anotherValue: unknown) {
-        return value === anotherValue;
-    };
-}
