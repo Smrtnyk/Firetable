d6fc4b5d89b6347e07e3de9923a48a784eb74eda
Smrtnyk <milankeser@outlook.de>
2023-11-06 09:14:10 +0100

    frontend(useFloors): rewrite composable

--- packages/frontend/src/composables/useFloors.ts
+++ packages/frontend/src/composables/useFloors.ts
@@ -1,7 +1,7 @@
 import { showErrorMessage } from "src/helpers/ui-helpers";
 import { onUnmounted, ref } from "vue";
 import { floorsCollection } from "@firetable/backend";
-import { FloorDoc } from "@firetable/types";
+import { FloorDoc, PropertyDoc } from "@firetable/types";
 import { usePropertiesStore } from "src/stores/usePropertiesStore";
 import { query, where, onSnapshot } from "firebase/firestore";
 
@@ -11,69 +11,67 @@ export type PropertyFloors = {
     organisationId: string;
     floors: FloorDoc[];
 };
+
 export type UsePropertyFloors = Record<string, PropertyFloors>;
 
 export function useFloors() {
-    const floors = ref<Record<string, PropertyFloors>>({});
+    const floors = ref<UsePropertyFloors>({});
     const isLoading = ref(true);
     const propertiesStore = usePropertiesStore();
+    const unsubscribes: (() => void)[] = [];
 
-    const loadingPromise = propertiesStore
-        .getPropertiesOfCurrentUser()
-        .then((properties) => {
-            console.log("got properties", properties);
-            const unsubscribes: (() => void)[] = [];
-            const fetchPromises: Promise<void>[] = [];
+    onUnmounted(() => {
+        unsubscribes.forEach((unsubscribe) => unsubscribe());
+    });
 
-            properties.forEach((property) => {
-                const floorQuery = query(
-                    floorsCollection(property.organisationId, property.id),
-                    where("propertyId", "==", property.id),
-                );
+    async function fetchFloorsForProperty(property: PropertyDoc): Promise<void> {
+        const floorQuery = query(
+            floorsCollection(property.organisationId, property.id),
+            where("propertyId", "==", property.id),
+        );
 
-                const fetchPromise = new Promise<void>((resolve) => {
-                    const unsubscribe = onSnapshot(
-                        floorQuery,
-                        (snapshot) => {
-                            floors.value[property.id] = {
-                                propertyId: property.id,
-                                propertyName: property.name,
-                                organisationId: property.organisationId,
-                                floors: snapshot.docs.map(function (doc) {
-                                    console.log("got floor doc ", doc.data());
-                                    return {
-                                        ...doc.data(),
-                                        id: doc.id,
-                                    } as FloorDoc;
-                                }),
-                            };
-                            resolve();
-                        },
-                        (error) => {
-                            console.error("Error fetching floors:", error);
-                        },
-                    );
-                    unsubscribes.push(unsubscribe);
-                });
-
-                fetchPromises.push(fetchPromise);
-            });
-
-            return Promise.all(fetchPromises)
-                .then(() => (isLoading.value = false))
-                .catch(showErrorMessage)
-                .finally(() => {
-                    onUnmounted(() => {
-                        for (const unsubscribe of unsubscribes) {
-                            unsubscribe();
-                        }
-                    });
-                });
-        })
-        .catch((err) => {
-            showErrorMessage(err);
-            isLoading.value = false;
+        return new Promise<void>((resolve, reject) => {
+            const unsubscribe = onSnapshot(
+                floorQuery,
+                (snapshot) => {
+                    floors.value[property.id] = {
+                        propertyId: property.id,
+                        propertyName: property.name,
+                        organisationId: property.organisationId,
+                        floors: snapshot.docs.map(
+                            (doc) =>
+                                ({
+                                    ...doc.data(),
+                                    id: doc.id,
+                                }) as FloorDoc,
+                        ),
+                    };
+                    resolve();
+                },
+                (error) => {
+                    console.error("Error fetching floors:", error);
+                    reject(error);
+                },
+            );
+            unsubscribes.push(unsubscribe);
         });
+    }
+
+    async function fetchAllFloors(): Promise<void> {
+        try {
+            const properties = await propertiesStore.getPropertiesOfCurrentUser();
+            const fetchPromises = properties.map(fetchFloorsForProperty);
+
+            await Promise.all(fetchPromises);
+        } catch (error) {
+            showErrorMessage(error);
+        } finally {
+            isLoading.value = false;
+        }
+    }
+
+    // Start fetching floors immediately
+    const loadingPromise = fetchAllFloors();
 
     return { floors, isLoading, loadingPromise };
 }
