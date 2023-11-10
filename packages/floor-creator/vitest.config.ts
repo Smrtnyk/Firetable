7131811215f878fa6ea0225c6e0eba51d5f1153a
Smrtnyk <milankeser@outlook.de>
2023-11-10 21:48:57 +0100

    floor-creator: inline monorepo packages in vitest tests

--- packages/floor-creator/vitest.config.ts
+++ packages/floor-creator/vitest.config.ts
@@ -10,5 +10,8 @@ export default defineConfig({
             headless: true,
             name: "chrome",
         },
+        deps: {
+            inline: ["@firetable/types", "@firetable/utils"],
+        },
     },
 });
