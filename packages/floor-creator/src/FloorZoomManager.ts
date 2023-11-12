b94b1b4253dec4ea3322fe68eb03f1a085b65725
Smrtnyk <milankeser@outlook.de>
2023-11-12 20:22:14 +0100

    FloorZoomManager: greatly simplify code

--- packages/floor-creator/src/FloorZoomManager.ts
+++ packages/floor-creator/src/FloorZoomManager.ts
@@ -2,40 +2,22 @@ import { fabric } from "fabric";
 import { DEFAULT_ZOOM, ZOOM_INCREMENT } from "./constants.js";
 
 export class FloorZoomManager {
-    private canvas: fabric.Canvas;
-    private initialScale: number;
-    private initialViewportTransform: number[];
-    readonly maxZoom: number = DEFAULT_ZOOM * 3; // 3 times the default zoom
-    minZoom: number;
+    private minZoom: number;
+    private readonly maxZoom: number = DEFAULT_ZOOM * 3;
 
-    private pinchZoomScale: number | null = null;
-    private pinchZoomPoint: fabric.Point | null = null;
-
-    private zoomDelta = 0;
-    private zoomPoint: fabric.Point | null = null;
-    // Define animation properties
-    private startTime?: number;
-    private startZoom?: number;
-    private targetZoom?: number;
-
-    private centerPoint: fabric.Point;
-
-    constructor(canvas: fabric.Canvas, initialScale: number, initialViewportTransform: number[]) {
-        this.canvas = canvas;
+    constructor(
+        private canvas: fabric.Canvas,
+        private initialScale: number,
+        private initialViewportTransform: number[],
+    ) {
         this.initialScale = initialScale;
         this.initialViewportTransform = initialViewportTransform;
         this.minZoom = this.initialScale;
-
-        this.centerPoint = new fabric.Point(canvas.getWidth() / 2, canvas.getHeight() / 2);
     }
 
     setScale(newVal: number): void {
         this.initialScale = newVal;
         this.minZoom = this.initialScale;
-        this.centerPoint = new fabric.Point(
-            this.canvas.getWidth() / 2,
-            this.canvas.getHeight() / 2,
-        );
     }
 
     setInitialViewportTransform(newVal: number[]): void {
@@ -50,72 +32,15 @@ export class FloorZoomManager {
         this.adjustZoom(1 - ZOOM_INCREMENT, point);
     }
 
-    private update(): void {
-        // Apply zoom changes if there are any
-        if (this.zoomDelta !== 0 && this.zoomPoint) {
-            const newZoom = this.canvas.getZoom() * (1 + this.zoomDelta);
-            this.canvas.zoomToPoint(this.zoomPoint, newZoom);
-            // Reset the stored state after applying
-            this.zoomDelta = 0;
-            this.zoomPoint = null;
-        }
-
-        // Apply pinch zoom changes if there are any
-        if (this.pinchZoomScale !== null && this.pinchZoomPoint !== null) {
-            this.applyPinchZoom();
-        }
-
-        // Handle ongoing animation for smooth zooming
-        if (
-            this.targetZoom !== undefined &&
-            this.startZoom !== undefined &&
-            this.startTime !== undefined
-        ) {
-            const zoom = this.startZoom + (this.targetZoom - this.startZoom);
-            const point = this.getCenterPoint();
-
-            this.canvas.zoomToPoint(point, zoom);
-            this.startZoom = undefined;
-            this.targetZoom = undefined;
-        }
-
-        this.canvas.requestRenderAll();
-    }
-
-    private getCenterPoint(): fabric.Point {
-        this.centerPoint.x = this.canvas.getWidth() / 2;
-        this.centerPoint.y = this.canvas.getHeight() / 2;
-        return this.centerPoint;
-    }
-
     adjustZoom(scaleFactor: number, point: fabric.Point): void {
         const newZoom = this.canvas.getZoom() * scaleFactor;
         if (newZoom <= this.maxZoom && newZoom >= this.minZoom) {
-            // Set the animation start properties
-            this.startTime = performance.now();
-            this.startZoom = this.canvas.getZoom();
-            this.targetZoom = newZoom;
-            this.zoomPoint = point; // Use the current point as the zoom focal point
+            this.canvas.zoomToPoint(point, newZoom);
 
-            this.update();
+            this.canvas.requestRenderAll();
         } else if (newZoom < this.minZoom) {
             this.resetZoom();
         }
-    }
-
-    private applyPinchZoom(): void {
-        if (this.pinchZoomScale !== null && this.pinchZoomPoint !== null) {
-            const newZoom = this.canvas.getZoom() * this.pinchZoomScale;
-            this.zoomToPoint(this.pinchZoomPoint, newZoom);
-            this.pinchZoomScale = null;
-            this.pinchZoomPoint = null;
-        }
-    }
-
-    private zoomToPoint(point: fabric.Point, scaleFactor: number): void {
-        let newZoom = this.canvas.getZoom() * scaleFactor;
-        newZoom = Math.max(this.minZoom, Math.min(newZoom, this.maxZoom));
-        this.canvas.zoomToPoint(point, newZoom);
     }
 
     canZoomIn(): boolean {
@@ -126,11 +51,13 @@ export class FloorZoomManager {
         return this.canvas.getZoom() > this.minZoom;
     }
 
-    resetZoom(): void {
+    private resetZoom(): void {
         this.canvas.setViewportTransform([...this.initialViewportTransform]);
         this.canvas.setZoom(this.initialScale);
         this.canvas.requestRenderAll();
     }
 
-    destroy(): void {}
+    destroy(): void {
+        // empty for now
+    }
 }
