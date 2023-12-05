44c5f9d44139f979bd9e0a3fcf22dda6fe916693
Smrtnyk <milankeser@outlook.de>
2023-12-05 19:00:20 +0100

    chore: move change password cloud function to own file

---
+++ packages/functions/src/callable/change-password.ts
@@ -0,0 +1,32 @@
+import type { CallableRequest } from "firebase-functions/v2/https";
+import { auth } from "../init.js";
+import { HttpsError } from "firebase-functions/v2/https";
+import { logger } from "firebase-functions/v2";
+
+const MIN_PASSWORD_LENGTH = 6;
+
+export async function changePasswordFn(
+    req: CallableRequest<{ newPassword: string }>,
+): Promise<{ success: boolean }> {
+    if (!req.auth) {
+        logger.error("Unauthenticated user tried to change password.");
+        throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
+    }
+
+    const uid: string = req.auth.uid;
+    const newPassword: string = req.data.newPassword;
+
+    if (!newPassword || newPassword.length < MIN_PASSWORD_LENGTH) {
+        logger.error("Invalid password provided by user:", uid);
+        throw new HttpsError("invalid-argument", "Password must be at least 6 characters long!");
+    }
+
+    try {
+        await auth.updateUser(uid, { password: newPassword });
+        logger.log("Password changed successfully for user:", uid);
+        return { success: true };
+    } catch (error) {
+        logger.error("Failed to update the password for user:", uid, error);
+        throw new HttpsError("internal", "Failed to update the user password.");
+    }
+}
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -10,45 +10,21 @@ import { onUserDeletedFn } from "./trigger/on-user-deleted.js";
 import { onPropertyDeletedFn } from "./trigger/on-property-deleted.js";
 import { onPropertyDeletedCleanEvents } from "./trigger/on-property-deleted-clean-events.js";
 
-import { auth } from "./init.js";
+import { changePasswordFn } from "./callable/change-password.js";
 import { Collection } from "../types/types.js";
 
 import { onSchedule } from "firebase-functions/v2/scheduler";
-import { onRequest, onCall, HttpsError } from "firebase-functions/v2/https";
+import { onRequest, onCall } from "firebase-functions/v2/https";
 import { onDocumentDeleted } from "firebase-functions/v2/firestore";
-import { setGlobalOptions, logger } from "firebase-functions/v2";
+import { setGlobalOptions } from "firebase-functions/v2";
 
 setGlobalOptions({ region: "europe-west3" });
-
-const MIN_PASSWORD_LENGTH = 6;
 
 // Everything that has to do with events
 export const createEvent = onCall({ cors: true }, createEventFn);
 
 // Everything that has to do with auth
-export const changePassword = onCall<{ newPassword: string }>({ cors: true }, async (req) => {
-    if (!req.auth) {
-        logger.error("Unauthenticated user tried to change password.");
-        throw new HttpsError("unauthenticated", "The function must be called while authenticated.");
-    }
-
-    const uid: string = req.auth.uid;
-    const newPassword: string = req.data.newPassword;
-
-    if (!newPassword || newPassword.length < MIN_PASSWORD_LENGTH) {
-        logger.error("Invalid password provided by user:", uid);
-        throw new HttpsError("invalid-argument", "Password must be at least 6 characters long!");
-    }
-
-    try {
-        await auth.updateUser(uid, { password: newPassword });
-        logger.log("Password changed successfully for user:", uid);
-        return { success: true };
-    } catch (error) {
-        logger.error("Failed to update the password for user:", uid, error);
-        throw new HttpsError("internal", "Failed to update the user password.");
-    }
-});
+export const changePassword = onCall({ cors: true }, changePasswordFn);
 export const fetchUsersByRole = onCall({ cors: true }, fetchUsersByRoleFn);
 export const createUser = onCall({ cors: true }, createUserFn);
 export const updateUser = onCall({ cors: true }, updateUserFn);
