02aa57fdb6f33fd0de96838849ed1cbd2a00c75a
Smrtnyk <milankeser@outlook.de>
2023-12-13 22:14:21 +0100

    feat: add scroll to bottom button for logs

--- packages/frontend/src/components/admin/event/AdminEventLogs.vue
+++ packages/frontend/src/components/admin/event/AdminEventLogs.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import type { EventLog, EventLogsDoc } from "@firetable/types";
+import type { QScrollAreaProps } from "quasar";
 import { ADMIN } from "@firetable/types";
 import { format } from "date-fns";
 import { useAuthStore } from "src/stores/auth-store";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import { QScrollArea } from "quasar";
 
 interface Props {
     logsDoc: EventLogsDoc;
@@ -18,6 +20,8 @@ const logs = computed(() => {
         return log.creator.role !== ADMIN;
     });
 });
+const showScrollButton = ref(false);
+const logsContainer = ref<QScrollArea | undefined>();
 
 function formatTimestampToReadable(date: Date): string {
     return format(date, "dd-MM-yyyy HH:mm:ss");
@@ -52,19 +56,56 @@ function formatSubtitleForEventLog({ creator, timestamp }: EventLog): string {
     const userPart = `${creator.name} (${creator.email})`;
     return `${datePart}, by ${userPart}`;
 }
+
+function handleScroll(ev: Parameters<NonNullable<QScrollAreaProps["onScroll"]>>[0]): void {
+    showScrollButton.value = ev.verticalPercentage > 0.1 && ev.verticalPercentage < 0.9;
+}
+
+function scrollToBottom(): void {
+    logsContainer.value?.setScrollPosition(
+        "vertical",
+        logsContainer.value.getScroll().verticalSize,
+        1000,
+    );
+}
 </script>
 
 <template>
-    <q-timeline color="primary">
-        <q-timeline-entry
-            v-for="log of logs"
-            :key="log.timestamp.nanoseconds"
-            :subtitle="formatSubtitleForEventLog(log)"
-            :icon="getIconNameForLogEntry(log.message)"
-        >
-            <div>
-                {{ log.message }}
-            </div>
-        </q-timeline-entry>
-    </q-timeline>
+    <q-scroll-area ref="logsContainer" class="logs-container" @scroll="handleScroll">
+        <q-timeline color="primary">
+            <q-timeline-entry
+                v-for="log of logs"
+                :key="log.timestamp.nanoseconds"
+                :subtitle="formatSubtitleForEventLog(log)"
+                :icon="getIconNameForLogEntry(log.message)"
+            >
+                <div>
+                    {{ log.message }}
+                </div>
+            </q-timeline-entry>
+        </q-timeline>
+    </q-scroll-area>
+
+    <q-page-sticky
+        v-if="showScrollButton"
+        position="bottom"
+        :offset="[18, 18]"
+        class="scroll-to-bottom"
+    >
+        <q-btn @click="scrollToBottom" fab icon="chevron_down" color="secondary" />
+    </q-page-sticky>
 </template>
+
+<style lang="scss">
+.logs-container {
+    height: calc(100vh - 200px);
+    margin-bottom: 16px;
+
+    .q-scrollarea__container {
+        padding-left: 16px !important;
+    }
+}
+.scroll-to-bottom {
+    z-index: 999999;
+}
+</style>
