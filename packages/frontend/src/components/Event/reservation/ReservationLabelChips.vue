f551fc12af8a619aab788a7226a5adc5573eff9c
Smrtnyk <milankeser@outlook.de>
2024-01-06 12:24:50 +0100

    chore: don't show planned label

--- packages/frontend/src/components/Event/reservation/ReservationLabelChips.vue
+++ packages/frontend/src/components/Event/reservation/ReservationLabelChips.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
 import type { ReservationDoc } from "@firetable/types";
-import { computed } from "vue";
-import { isPlannedReservation } from "@firetable/types";
+import { isAWalkInReservation } from "@firetable/types";
 
 const props = defineProps<{
     reservation: ReservationDoc;
 }>();
-
-const reservationChipLabel = computed(() => {
-    return isPlannedReservation(props.reservation) ? "Planned" : "Walk-In";
-});
-
-const reservationChipColor = computed(() => {
-    return isPlannedReservation(props.reservation) ? "tertiary" : "quaternary";
-});
 </script>
 
 <template>
     <div class="row justify-end">
-        <q-chip :color="reservationChipColor" :label="reservationChipLabel" />
+        <q-chip v-if="isAWalkInReservation(props.reservation)" color="quaternary" label="Walk-In" />
         <q-chip
             color="accent"
             v-if="props.reservation.isVIP"
