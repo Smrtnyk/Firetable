a6446d654750493783653ddffab31a33e1dd39ef
Smrtnyk <milankeser@outlook.de>
2024-01-13 22:42:15 +0100

    refactor: simplify PageAdminOrganisationSettings.vue

---
+++ packages/frontend/src/components/admin/organisation-settings/SettingsSection.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+interface Props {
+    title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-xs-8 col-sm-10 column justify-center">
+            {{ props.title }}
+        </div>
+        <div class="col-xs-4 col-sm-2">
+            <slot />
+        </div>
+    </div>
+</template>
--- packages/frontend/src/pages/Admin/PageAdminOrganisationSettings.vue
+++ packages/frontend/src/pages/Admin/PageAdminOrganisationSettings.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { usePropertiesStore } from "src/stores/properties-store";
 import { computed, ref } from "vue";
-
-import FTTitle from "src/components/FTTitle.vue";
-import FTCenteredText from "src/components/FTCenteredText.vue";
 import { withLoading } from "src/helpers/ui-helpers";
 import { updateOrganisationSettings } from "@firetable/backend";
-import { QPopupProxy } from "quasar";
+
+import SettingsSection from "src/components/admin/organisation-settings/SettingsSection.vue";
+import FTTitle from "src/components/FTTitle.vue";
+import FTCenteredText from "src/components/FTCenteredText.vue";
 
 interface Props {
     organisationId: string;
@@ -29,6 +29,25 @@ const hasSettingsChanged = computed(() => {
 const saveSettings = withLoading(() => {
     return updateOrganisationSettings(props.organisationId, editableSettings.value);
 });
+
+const colorsSettings = [
+    {
+        title: "Reservation pending color",
+        key: "reservationPendingColor",
+    },
+    {
+        title: "Reservation arrived color",
+        key: "reservationArrivedColor",
+    },
+    {
+        title: "Reservation confirmed color",
+        key: "reservationConfirmedColor",
+    },
+    {
+        title: "Reservation cancelled color",
+        key: "reservationCancelledColor",
+    },
+];
 </script>
 
 <template>
@@ -50,153 +69,74 @@ const saveSettings = withLoading(() => {
         <q-card class="ft-card q-pa-md q-mb-md">
             <FTCenteredText>Property</FTCenteredText>
 
-            <div class="row">
-                <div class="col-xs-8 col-sm-10">Property card aspect ratio</div>
-
-                <div class="col-xs-4 col-sm-2">
-                    <q-select
-                        rounded
-                        standout
-                        v-model="editableSettings.property.propertyCardAspectRatio"
-                        :options="aspectRatioOptions"
-                    />
-                </div>
-            </div>
+            <SettingsSection title="Property card aspect ratio">
+                <q-select
+                    rounded
+                    standout
+                    v-model="editableSettings.property.propertyCardAspectRatio"
+                    :options="aspectRatioOptions"
+                />
+            </SettingsSection>
         </q-card>
 
         <q-card class="ft-card q-pa-md q-mb-md">
             <FTCenteredText>Event</FTCenteredText>
 
             <div class="column q-col-gutter-md">
-                <div class="row">
-                    <div class="col-xs-8 col-sm-10">Event start time</div>
-
-                    <div class="col-xs-4 col-sm-2">
-                        <q-input
-                            :model-value="editableSettings.event.eventStartTime24HFormat"
-                            rounded
-                            standout
-                            readonly
-                        >
-                            <template #append>
-                                <q-icon name="clock" class="cursor-pointer" />
-                                <q-popup-proxy transition-show="scale" transition-hide="scale">
-                                    <q-time
-                                        v-model="editableSettings.event.eventStartTime24HFormat"
-                                        format="24h"
-                                    />
-                                </q-popup-proxy>
-                            </template>
-                        </q-input>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-8 col-sm-10">Event duration in hours</div>
-
-                    <div class="col-xs-4 col-sm-2">
-                        <q-input
-                            rounded
-                            standout
-                            v-model.number="editableSettings.event.eventDurationInHours"
-                        />
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-8 col-sm-10">Event card aspect ratio</div>
-
-                    <div class="col-xs-4 col-sm-2">
-                        <q-select
-                            rounded
-                            standout
-                            v-model="editableSettings.event.eventCardAspectRatio"
-                            :options="aspectRatioOptions"
-                        />
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-10">Reserved pending color</div>
-
-                    <div class="col-2">
-                        <q-btn
-                            class="full-width"
-                            title="Reservation pending color"
-                            :style="{
-                                'background-color': editableSettings.event.reservationPendingColor,
-                            }"
-                        >
-                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
-                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                                <q-color v-model="editableSettings.event.reservationPendingColor" />
-                            </q-popup-proxy>
-                        </q-btn>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-10">Reserved arrived color</div>
-
-                    <div class="col-2">
-                        <q-btn
-                            class="full-width"
-                            title="Reservation arrived color"
-                            :style="{
-                                'background-color': editableSettings.event.reservationArrivedColor,
-                            }"
-                        >
-                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
-                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                                <q-color v-model="editableSettings.event.reservationArrivedColor" />
-                            </q-popup-proxy>
-                        </q-btn>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-10">Reserved confirmed color</div>
-
-                    <div class="col-2">
-                        <q-btn
-                            class="full-width"
-                            title="Reservation confirmed color"
-                            :style="{
-                                'background-color':
-                                    editableSettings.event.reservationConfirmedColor,
-                            }"
-                        >
-                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
-                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                                <q-color
-                                    v-model="editableSettings.event.reservationConfirmedColor"
+                <SettingsSection title="Event start date">
+                    <q-input
+                        :model-value="editableSettings.event.eventStartTime24HFormat"
+                        rounded
+                        standout
+                        readonly
+                    >
+                        <template #append>
+                            <q-icon name="clock" class="cursor-pointer" />
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                <q-time
+                                    v-model="editableSettings.event.eventStartTime24HFormat"
+                                    format="24h"
                                 />
                             </q-popup-proxy>
-                        </q-btn>
-                    </div>
-                </div>
+                        </template>
+                    </q-input>
+                </SettingsSection>
 
-                <div class="row">
-                    <div class="col-10">Reserved cancelled color</div>
+                <SettingsSection title="Event duration in hours">
+                    <q-input
+                        rounded
+                        standout
+                        v-model.number="editableSettings.event.eventDurationInHours"
+                    />
+                </SettingsSection>
 
-                    <div class="col-2">
-                        <q-btn
-                            class="full-width"
-                            title="Reservation cancelled color"
-                            :style="{
-                                'background-color':
-                                    editableSettings.event.reservationCancelledColor,
-                            }"
-                        >
-                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
-                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                                <q-color
-                                    v-model="editableSettings.event.reservationCancelledColor"
-                                />
-                            </q-popup-proxy>
-                        </q-btn>
-                    </div>
-                </div>
+                <SettingsSection title="Event card aspect ratio">
+                    <q-select
+                        rounded
+                        standout
+                        v-model="editableSettings.event.eventCardAspectRatio"
+                        :options="aspectRatioOptions"
+                    />
+                </SettingsSection>
+
+                <SettingsSection
+                    v-for="colorSetting in colorsSettings"
+                    :key="colorSetting.key"
+                    :title="colorSetting.title"
+                >
+                    <q-btn
+                        class="full-width"
+                        :title="colorSetting.title"
+                        :style="{
+                            'background-color': editableSettings.event[colorSetting.key],
+                        }"
+                    >
+                        <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                        <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                            <q-color v-model="editableSettings.event[colorSetting.key]" />
+                        </q-popup-proxy>
+                    </q-btn>
+                </SettingsSection>
             </div>
         </q-card>
     </div>
