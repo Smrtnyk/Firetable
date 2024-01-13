42b68e1536924657d1145053ebfd9276def3c4f8
Smrtnyk <milankeser@outlook.de>
2024-01-13 22:30:55 +0100

    feat: add reservation colors to be configurable in PageAdminOrganisationSettings.vue

--- packages/frontend/src/pages/Admin/PageAdminOrganisationSettings.vue
+++ packages/frontend/src/pages/Admin/PageAdminOrganisationSettings.vue
@@ -6,6 +6,7 @@ import FTTitle from "src/components/FTTitle.vue";
 import FTCenteredText from "src/components/FTCenteredText.vue";
 import { withLoading } from "src/helpers/ui-helpers";
 import { updateOrganisationSettings } from "@firetable/backend";
+import { QPopupProxy } from "quasar";
 
 interface Props {
     organisationId: string;
@@ -50,9 +51,9 @@ const saveSettings = withLoading(() => {
             <FTCenteredText>Property</FTCenteredText>
 
             <div class="row">
-                <div class="col-10">Property card aspect ratio</div>
+                <div class="col-xs-8 col-sm-10">Property card aspect ratio</div>
 
-                <div class="col-2">
+                <div class="col-xs-4 col-sm-2">
                     <q-select
                         rounded
                         standout
@@ -68,9 +69,9 @@ const saveSettings = withLoading(() => {
 
             <div class="column q-col-gutter-md">
                 <div class="row">
-                    <div class="col-10">Event start time</div>
+                    <div class="col-xs-8 col-sm-10">Event start time</div>
 
-                    <div class="col-2">
+                    <div class="col-xs-4 col-sm-2">
                         <q-input
                             :model-value="editableSettings.event.eventStartTime24HFormat"
                             rounded
@@ -91,9 +92,9 @@ const saveSettings = withLoading(() => {
                 </div>
 
                 <div class="row">
-                    <div class="col-10">Event duration in hours</div>
+                    <div class="col-xs-8 col-sm-10">Event duration in hours</div>
 
-                    <div class="col-2">
+                    <div class="col-xs-4 col-sm-2">
                         <q-input
                             rounded
                             standout
@@ -103,15 +104,97 @@ const saveSettings = withLoading(() => {
                 </div>
 
                 <div class="row">
-                    <div class="col-10">Event card aspect ratio</div>
+                    <div class="col-xs-8 col-sm-10">Event card aspect ratio</div>
 
-                    <div class="col-2">
+                    <div class="col-xs-4 col-sm-2">
                         <q-select
                             rounded
                             standout
                             v-model="editableSettings.event.eventCardAspectRatio"
                             :options="aspectRatioOptions"
                         />
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-10">Reserved pending color</div>
+
+                    <div class="col-2">
+                        <q-btn
+                            class="full-width"
+                            title="Reservation pending color"
+                            :style="{
+                                'background-color': editableSettings.event.reservationPendingColor,
+                            }"
+                        >
+                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                                <q-color v-model="editableSettings.event.reservationPendingColor" />
+                            </q-popup-proxy>
+                        </q-btn>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-10">Reserved arrived color</div>
+
+                    <div class="col-2">
+                        <q-btn
+                            class="full-width"
+                            title="Reservation arrived color"
+                            :style="{
+                                'background-color': editableSettings.event.reservationArrivedColor,
+                            }"
+                        >
+                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                                <q-color v-model="editableSettings.event.reservationArrivedColor" />
+                            </q-popup-proxy>
+                        </q-btn>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-10">Reserved confirmed color</div>
+
+                    <div class="col-2">
+                        <q-btn
+                            class="full-width"
+                            title="Reservation confirmed color"
+                            :style="{
+                                'background-color':
+                                    editableSettings.event.reservationConfirmedColor,
+                            }"
+                        >
+                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                                <q-color
+                                    v-model="editableSettings.event.reservationConfirmedColor"
+                                />
+                            </q-popup-proxy>
+                        </q-btn>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-10">Reserved cancelled color</div>
+
+                    <div class="col-2">
+                        <q-btn
+                            class="full-width"
+                            title="Reservation cancelled color"
+                            :style="{
+                                'background-color':
+                                    editableSettings.event.reservationCancelledColor,
+                            }"
+                        >
+                            <q-icon name="color-picker" class="cursor-pointer q-ma-none" />
+                            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                                <q-color
+                                    v-model="editableSettings.event.reservationCancelledColor"
+                                />
+                            </q-popup-proxy>
+                        </q-btn>
                     </div>
                 </div>
             </div>
