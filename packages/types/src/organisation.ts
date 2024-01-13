61fa3778c5ba15fd52201083293d1b7169b20b4b
Smrtnyk <milankeser@outlook.de>
2024-01-13 12:54:38 +0100

    feat: add settings to organisation doc types

--- packages/types/src/organisation.ts
+++ packages/types/src/organisation.ts
@@ -1,5 +1,19 @@
+export type AspectRatio = "1" | "16:9";
+
 export interface OrganisationDoc {
     id: string;
     name: string;
     maxAllowedProperties: number;
+    settings?: OrganisationSettings;
+}
+
+export interface OrganisationSettings {
+    property: {
+        propertyCardAspectRatio: AspectRatio;
+    };
+    event: {
+        eventCardAspectRatio: AspectRatio;
+        eventStartTime24HFormat: string;
+        eventDurationInHours: number;
+    };
 }
