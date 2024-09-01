26638cef2bd10760735b8a6400884cf06c6540b5
Smrtnyk <milankeser@outlook.de>
2024-09-01 12:02:15 +0200

    chore: up deps

--- packages/floor-creator/src/FloorEditor.ts
+++ packages/floor-creator/src/FloorEditor.ts
@@ -15,7 +15,6 @@ import { EditorEventManager } from "./event-manager/EditorEventManager.js";
 import { calculateCanvasScale } from "./utils.js";
 import { CommandInvoker } from "./command/CommandInvoker.js";
 import { EventEmitter } from "./event-emitter/EventEmitter.js";
-// @ts-expect-error -- fabric screwed up the exports
 import { initAligningGuidelines } from "fabric/extensions";
 
 type FloorEditorEvents = {
