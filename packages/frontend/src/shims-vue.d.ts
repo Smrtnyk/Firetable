60193cf7f0ddbb1795f0cd0fa6a6a3c7ab89de80
Smrtnyk <milankeser@outlook.de>
2024-10-07 19:35:38 +0200

    chore: update shims-vue.d.ts

--- packages/frontend/src/shims-vue.d.ts
+++ packages/frontend/src/shims-vue.d.ts
@@ -3,7 +3,7 @@
 
 // Mocks all files ending in `.vue` showing them as plain Vue instances
 declare module "*.vue" {
-    import type { DefineComponent } from "vue";
-    const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>;
+    import type { defineComponent } from "vue";
+    const component: ReturnType<typeof defineComponent>;
     export default component;
 }
