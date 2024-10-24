2ea53bc726fffbbb26ae9eb3c5cd4d75678d0a50
Smrtnyk <milankeser@outlook.de>
2024-10-24 18:34:09 +0200

    chore(frontend): improve FTTitle

--- packages/frontend/src/components/FTTitle.vue
+++ packages/frontend/src/components/FTTitle.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { isMobile } from "src/global-reactives/screen-detection";
+
 interface Props {
     title: string;
     subtitle?: string;
 }
 const { title, subtitle } = defineProps<Props>();
+
+const titleClass = computed(() => {
+    return isMobile.value ? "text-h6" : "text-h4";
+});
+
+const subtitleClass = computed(() => {
+    return isMobile.value ? "text-subtitle2" : "text-subtitle1";
+});
 </script>
 
 <template>
     <q-card class="ft-card q-pa-sm q-mb-md">
         <div class="FTTitle items-center">
             <div>
-                <h3 role="heading" aria-level="3" class="ft-title q-ma-none">{{ title }}</h3>
-                <h6 v-if="subtitle" class="ft-subtitle q-ma-none">
+                <h3 role="heading" aria-level="3" :class="['ft-title', 'q-ma-none', titleClass]">
+                    {{ title }}
+                </h3>
+                <h6 v-if="subtitle" :class="['ft-subtitle', 'q-ma-none', subtitleClass]">
                     {{ subtitle }}
                 </h6>
             </div>
@@ -29,7 +42,6 @@ const { title, subtitle } = defineProps<Props>();
     grid-template-columns: 1fr auto;
 }
 .ft-title {
-    font-size: 2rem;
     color: #2d3748;
     font-weight: 900;
     padding-left: 0.75rem;
@@ -40,7 +52,6 @@ const { title, subtitle } = defineProps<Props>();
 }
 .ft-subtitle {
     color: #8795a4;
-    font-size: 1rem;
     font-weight: 400;
     padding-left: 0.75rem;
 }
