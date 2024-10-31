6be987f69bd2d226d045bd51c9c552f6af154d3c
Smrtnyk <milankeser@outlook.de>
2024-10-31 21:19:39 +0100

    chore(frontend): dedupe types

--- packages/frontend/src/boot/event-emitter.ts
+++ packages/frontend/src/boot/event-emitter.ts
@@ -3,52 +3,39 @@ import type { EventOwner } from "@firetable/backend";
 import type { BaseTable } from "@firetable/floor-creator";
 import { EventEmitter } from "@posva/event-emitter";
 
-type TransferEventData = {
+interface BaseEventData {
+    eventOwner: EventOwner;
+    event: EventDoc;
+}
+
+interface BaseReservationEventData extends BaseEventData {
+    reservation: ReservationDoc;
+}
+
+export interface TransferEventData {
     fromTable: BaseTable;
     toTable: BaseTable;
     eventOwner: EventOwner;
     fromFloor?: string;
     toFloor?: string;
     targetReservation: ReservationDoc | undefined;
-};
+}
 
 type Events = {
-    "reservation:created": {
+    "reservation:created": BaseEventData & {
         reservation: Reservation;
-        eventOwner: EventOwner;
-        event: EventDoc;
     };
-    "reservation:updated": {
-        reservation: ReservationDoc;
-        eventOwner: EventOwner;
-        event: EventDoc;
-    };
-    "reservation:deleted": {
-        reservation: ReservationDoc;
-        eventOwner: EventOwner;
-        event: EventDoc;
-    };
-    "reservation:deleted:soft": {
-        reservation: ReservationDoc;
-        eventOwner: EventOwner;
-        event: EventDoc;
-    };
+    "reservation:updated": BaseReservationEventData;
+    "reservation:deleted": BaseReservationEventData;
+    "reservation:deleted:soft": BaseReservationEventData;
     "reservation:copied": {
         sourceReservation: Reservation;
         targetTable: BaseTable;
         eventOwner: EventOwner;
     };
     "reservation:transferred": TransferEventData;
-    "reservation:arrived": {
-        reservation: ReservationDoc;
-        eventOwner: EventOwner;
-        event: EventDoc;
-    };
-    "reservation:cancelled": {
-        reservation: ReservationDoc;
-        eventOwner: EventOwner;
-        event: EventDoc;
-    };
+    "reservation:arrived": BaseReservationEventData;
+    "reservation:cancelled": BaseReservationEventData;
 };
 
 export const eventEmitter = new EventEmitter<Events>();
