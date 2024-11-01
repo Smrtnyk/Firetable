77ce651e9b9b32b9b3a37a01592724a25d2d1250
Smrtnyk <milankeser@outlook.de>
2024-11-01 19:42:39 +0100

    chore: add rum

--- packages/frontend/src/boot/rum.ts
+++ packages/frontend/src/boot/rum.ts
@@ -4,18 +4,19 @@ import { AppLogger } from "src/logger/FTLogger";
 
 export default boot(async function ({ app, router }) {
     try {
-        const sentryCreds = await import("./sentry.json");
+        const sentryConfig = await fetch(new URL("./sentry.json", import.meta.url).href);
+        const { dsn } = await sentryConfig.json();
 
         Sentry.init({
             app,
-            dsn: sentryCreds.dsn,
+            dsn,
             tracesSampleRate: 1,
             environment: import.meta.env.PROD ? "production" : "development",
             integrations: [Sentry.browserTracingIntegration({ router })],
             trackComponents: true,
             hooks: ["mount", "update", "unmount"],
         });
-    } catch (e) {
-        AppLogger.error("Failed to initialize Sentry", e);
+    } catch (error) {
+        AppLogger.error("Failed to initialize Sentry - continuing without monitoring", error);
     }
 });
