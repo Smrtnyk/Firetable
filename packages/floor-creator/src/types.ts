4ed652433d2a4db7513f1492161834b97506c5c3
Smrtnyk <milankeser@outlook.de>
2024-11-04 10:11:24 +0100

    chore(floor-creator/frontend): create floor element click handler

--- packages/floor-creator/src/types.ts
+++ packages/floor-creator/src/types.ts
@@ -1,8 +1,13 @@
 import type { RectTable } from "./elements/RectTable.js";
 import type { RoundTable } from "./elements/RoundTable.js";
 import type { FabricObject } from "fabric";
+import type { FloorViewer } from "./FloorViewer.js";
 
 export type ToTuple<T> = T extends [] | [unknown, ...unknown[]] ? T : [T];
+export type FloorElementClickHandler = (
+    floor: FloorViewer,
+    element: FloorEditorElement | undefined,
+) => Promise<void> | void;
 
 export interface FloorCreationOptions {
     canvas: HTMLCanvasElement;
