75f13731f01d29a2d51178100cfb291d5a8e5e15
Smrtnyk <milankeser@outlook.de>
2024-11-05 12:32:43 +0100

    chore: force newer jsdom

--- packages/functions/vitest.config.ts
+++ packages/functions/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
+        environment: "node",
         cache: false,
         globals: true,
         include: ["**/*.spec.ts"],
