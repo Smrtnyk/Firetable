a3496f6e846872efd1ae94c8d45cf2fd00f7a2c7
Smrtnyk <milankeser@outlook.de>
2025-01-04 17:12:19 +0100

    chore(functions): replace yargs with cac

--- packages/functions/seeds/scripts/seed.ts
+++ packages/functions/seeds/scripts/seed.ts
@@ -8,32 +8,25 @@ import { verifyEmulatorConnection } from "../config.js";
 import { ReservationSeeder } from "../ReservationSeeder.js";
 import { logger } from "../logger.js";
 import { faker } from "@faker-js/faker";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import { cac } from "cac";
 
 await verifyEmulatorConnection();
 
-const argv = yargs(hideBin(process.argv))
-    .option("organisations", {
-        alias: "o",
-        description: "Number of organisations to seed",
-        type: "number",
-        default: 1,
-    })
-    .option("with-admin", {
-        alias: "a",
-        description: "Create an admin user",
-        type: "boolean",
+const cli = cac("seed");
+
+cli.option("-o, --organisations <number>", "Number of organisations to seed", {
+    default: 1,
+})
+    .option("-a, --with-admin", "Create an admin user", {
         default: false,
     })
-    .help()
-    .alias("help", "h")
-    .version(false)
-    .parseSync();
+    .help();
+
+const { options } = cli.parse();
 
 async function seed(): Promise<void> {
     const startTime = Date.now();
-    logger.info(`🌱 Starting seeding process (Creating ${argv.organisations} organisations)...`);
+    logger.info(`🌱 Starting seeding process (Creating ${options.organisations} organisations)...`);
 
     try {
         const organisationSeeder = new OrganisationSeeder();
@@ -44,10 +37,10 @@ async function seed(): Promise<void> {
         const guestSeeder = new GuestSeeder();
         const reservationSeeder = new ReservationSeeder();
 
-        for (let i = 0; i < argv.organisations; i++) {
+        for (let i = 0; i < options.organisations; i++) {
             const orgId = `org-${(i + 1).toString().padStart(2, "0")}`;
             const organisation = await organisationSeeder.seedOne(orgId);
-            logger.organization(i + 1, argv.organisations, organisation.name);
+            logger.organization(i + 1, options.organisations, organisation.name);
 
             const propertyCount = faker.number.int({ min: 1, max: 5 });
             const properties = await propertySeeder.seedForOrganisation(
@@ -70,7 +63,7 @@ async function seed(): Promise<void> {
             );
         }
 
-        if (argv.withAdmin) {
+        if (options.withAdmin) {
             await userSeeder.createAdminUser();
         }
 
@@ -82,5 +75,10 @@ async function seed(): Promise<void> {
         process.exit(1);
     }
 }
+
+cli.on("command:*", function () {
+    console.error("Invalid command");
+    process.exit(1);
+});
 
 await seed();
