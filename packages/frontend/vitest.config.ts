6bd6e1acb2b88b3a53a90991dc2f090fb9e818b8
Smrtnyk <milankeser@outlook.de>
2025-04-12 19:28:54 +0200

    chore: up deps

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
                 }
             }
         },
+        pool: "threads",
         clearMocks: true,
         reporters: ["default"],
         watch: false,
