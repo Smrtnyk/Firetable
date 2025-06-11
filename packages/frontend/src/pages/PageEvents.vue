1284829114b91bb58781927b042a563b0310f526
Smrtnyk <milankeser@outlook.de>
2025-06-11 11:37:06 +0200

    chore(frontend): instead of redirect open the dialog to create event on PageEvents

--- packages/frontend/src/pages/PageEvents.vue
+++ packages/frontend/src/pages/PageEvents.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-import type { EventDoc } from "@firetable/types";
+import type { CreateEventPayload, EventDoc } from "@firetable/types";
 import type { EventOwner } from "src/db";
 
 import { limit, orderBy, where } from "firebase/firestore";
 import { storeToRefs } from "pinia";
-import { Loading } from "quasar";
+import { Loading, useQuasar } from "quasar";
+import EventCreateForm from "src/components/admin/event/EventCreateForm.vue";
 import EventCardList from "src/components/Event/EventCardList.vue";
 import FTBtn from "src/components/FTBtn.vue";
 import FTCenteredText from "src/components/FTCenteredText.vue";
+import FTDialog from "src/components/FTDialog.vue";
 import FTTitle from "src/components/FTTitle.vue";
+import { useDialog } from "src/composables/useDialog";
 import { createQuery, useFirestoreCollection } from "src/composables/useFirestore";
+import { useFloors } from "src/composables/useFloors";
 import { ONE_HOUR } from "src/constants";
-import { eventsCollection } from "src/db";
+import { createNewEvent, eventsCollection } from "src/db";
+import { tryCatchLoadingWrapper } from "src/helpers/ui-helpers";
 import { parseAspectRatio } from "src/helpers/utils";
 import { usePermissionsStore } from "src/stores/permissions-store";
 import { usePropertiesStore } from "src/stores/properties-store";
 import { computed, onMounted, watch } from "vue";
+import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 
 interface Props {
@@ -23,8 +29,8 @@ interface Props {
     propertyId: string;
 }
 
-const router = useRouter();
 const { organisationId, propertyId } = defineProps<Props>();
+const { t } = useI18n();
 const propertiesStore = usePropertiesStore();
 const { canCreateEvents } = storeToRefs(usePermissionsStore());
 
@@ -33,14 +39,22 @@ const eventOwner: EventOwner = {
     organisationId,
     propertyId,
 };
-
+const router = useRouter();
+const quasar = useQuasar();
+const subscriptionSettings = computed(function () {
+    return propertiesStore.getOrganisationSubscriptionSettingsById(organisationId);
+});
 const propertySettings = computed(function () {
     return propertiesStore.getPropertySettingsById(propertyId);
 });
 
+const { createDialog } = useDialog();
+
 const cardsAspectRatio = computed(function () {
     return parseAspectRatio(propertySettings.value.event.eventCardAspectRatio);
 });
+
+const { floors, isLoading: isFloorsLoading } = useFloors(propertyId, organisationId);
 
 const { data: events, pending: isLoading } = useFirestoreCollection<EventDoc>(
     createQuery<EventDoc>(
@@ -55,9 +69,11 @@ const { data: events, pending: isLoading } = useFirestoreCollection<EventDoc>(
         limit(10),
     ),
 );
-
+const isAnyLoading = computed(function () {
+    return isFloorsLoading.value || isLoading.value;
+});
 watch(
-    isLoading,
+    isAnyLoading,
     function (newIsLoading) {
         if (newIsLoading) {
             Loading.show();
@@ -74,10 +90,39 @@ onMounted(async function () {
     }
 });
 
-function redirectToAdminEvents(): void {
-    router.push({
-        name: "adminEvents",
-        params: { organisationId, propertyId },
+async function onCreateEvent(eventData: CreateEventPayload): Promise<void> {
+    await tryCatchLoadingWrapper({
+        async hook() {
+            await createNewEvent(eventData);
+            quasar.notify(t("PageAdminEvents.eventCreatedNotificationMessage"));
+        },
+    });
+}
+
+function showEventForm(event?: EventDoc): void {
+    const dialog = createDialog({
+        component: FTDialog,
+        componentProps: {
+            component: EventCreateForm,
+            componentPropsObject: {
+                event,
+                eventStartHours: propertySettings.value.event.eventStartTime24HFormat,
+                floors: floors.value,
+                maxFloors: subscriptionSettings.value.maxFloorPlansPerEvent,
+                organisationId,
+                propertyId,
+                propertyName: propertiesStore.getPropertyNameById(propertyId),
+                propertyTimezone: propertySettings.value.timezone,
+            },
+            listeners: {
+                create(eventData: CreateEventPayload) {
+                    onCreateEvent(eventData);
+                    dialog.hide();
+                },
+            },
+            maximized: false,
+            title: t("PageAdminEvents.createNewEventDialogTitle"),
+        },
     });
 }
 </script>
@@ -90,7 +135,7 @@ function redirectToAdminEvents(): void {
                     class="button-gradient"
                     icon="fa fa-plus"
                     rounded
-                    @click="redirectToAdminEvents"
+                    @click="() => showEventForm()"
                 />
             </template>
         </FTTitle>
